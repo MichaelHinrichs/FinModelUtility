a778bfa87e81c4860b793fc52871ebaea3c5ab26
MeltyPlayer <mastergamepro@hotmail.com>
2021-09-23 15:23:52 -0500

    Fixed issue where the logging logic wasn't getting included in the repo.

---
+++ MKDS Course Modifier/FinModelExporter/src/log/Logging.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace fin.log {
+  public class Logging {
+    private static bool VERBOSE_;
+
+    public static void Initialize(bool verbose) {
+      Logging.VERBOSE_ = verbose;
+    }
+
+    public static ILogger Create<T>()
+      => Logging.VERBOSE_
+             ? new Logger<T>(
+                 LoggerFactory.Create(builder => {
+                   builder.AddConsole();
+                   builder.AddDebug();
+                 }))
+             : new NullLogger<T>();
+  }
+}
