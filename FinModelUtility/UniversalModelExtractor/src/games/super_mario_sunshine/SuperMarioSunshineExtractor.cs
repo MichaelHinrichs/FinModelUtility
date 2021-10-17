8d756a7f1181c20164ca6cfa0d6c41b1fbc2b0a1
MeltyPlayer <mastergamepro@hotmail.com>
2021-10-16 22:32:06 -0500

    Fixed issue where the Super Mario Sunshine exporter wasn't actually working right, added support for Mario's animations.

--- FinModelUtility/UniversalModelExtractor/src/games/super_mario_sunshine/SuperMarioSunshineExtractor.cs
+++ FinModelUtility/UniversalModelExtractor/src/games/super_mario_sunshine/SuperMarioSunshineExtractor.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-using bmd.api;
+﻿using bmd.api;
 
 using fin.io;
+using fin.log;
+using fin.util.asserts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using uni.platforms;
 using uni.platforms.gcn;
-using uni.platforms.gcn.tools;
+using uni.src.msg;
 using uni.util.io;
 
 namespace uni.games.super_mario_sunshine {
   public class SuperMarioSunshineExtractor {
+    private readonly ILogger logger_ =
+        Logging.Create<SuperMarioSunshineExtractor>();
+
     public void ExtractAll() {
       var superMarioSunshineRom =
           DirectoryConstants.ROMS_DIRECTORY.TryToGetFile(
@@ -20,52 +26,23 @@ namespace uni.games.super_mario_sunshine {
       var fileHierarchy =
           new GcnFileHierarchyExtractor().ExtractFromRom(superMarioSunshineRom);
 
-      this.Debug_(fileHierarchy);
-
-      //this.ExtractMario_(fileHierarchy);
-      //this.ExtractScenes_(fileHierarchy);
+      this.ExtractMario_(fileHierarchy);
+      this.ExtractScenes_(fileHierarchy);
     }
 
     private void ExtractMario_(IFileHierarchy fileHierarchy) {
       var marioSubdir =
           fileHierarchy.Root.TryToGetSubdir(@"data\mario");
-      var marioBmd = marioSubdir.TryToGetSubdir("bmd")
-                                .Files.Single(
-                                    file => file.Name == "ma_mdl1.bmd");
+      var bcxFiles = marioSubdir.TryToGetSubdir("bck")
+                                .Files.Where(
+                                    file => file.Name.StartsWith("ma_"))
+                                .Select(file => file.Impl)
+                                .ToArray();
+      var bmdFile = marioSubdir.TryToGetSubdir("bmd")
+                               .Files.Single(
+                                   file => file.Name == "ma_mdl1.bmd");
 
-      var outputDirectory =
-          GameFileHierarchyUtil.GetOutputDirectoryForDirectory(marioSubdir);
-
-      new ManualBmd2FbxApi().Process(outputDirectory,
-                                     new[] {
-                                         marioBmd.FullName
-                                     },
-                                     new List<string>(),
-                                     new List<string>(),
-                                     true,
-                                     60);
-    }
-
-    private void Debug_(IFileHierarchy fileHierarchy) {
-      var sceneSubdir =
-          fileHierarchy.Root.TryToGetSubdir(@"data\scene");
-
-      var specificSubdir = sceneSubdir.TryToGetSubdir("bianco1_scene");
-      //var specificSubdir = sceneSubdir.TryToGetSubdir("dolpic0_scene");
-
-      var bmd = specificSubdir.TryToGetSubdir(@"map\map")
-                              .Files.Single(file => file.Name == "map.bmd");
-
-      var outputDirectory =
-          GameFileHierarchyUtil.GetOutputDirectoryForDirectory(specificSubdir);
-
-      new ManualBmd2FbxApi().Process(outputDirectory,
-                                     new[] {
-                                         bmd.FullName
-                                     },
-                                     new List<string>(),
-                                     new List<string>(),
-                                     true);
+      this.ExtractModels_(marioSubdir, new[] {bmdFile}, bcxFiles);
     }
 
     private void ExtractScenes_(IFileHierarchy fileHierarchy) {
@@ -73,20 +50,75 @@ namespace uni.games.super_mario_sunshine {
           fileHierarchy.Root.TryToGetSubdir(@"data\scene");
 
       foreach (var subdir in sceneSubdir.Subdirs) {
-        var bmd = subdir.TryToGetSubdir(@"map\map")
-                        .Files.Single(file => file.Name == "map.bmd");
-
-        var outputDirectory =
-            GameFileHierarchyUtil.GetOutputDirectoryForDirectory(subdir);
-
-        new ManualBmd2FbxApi().Process(outputDirectory,
-                                       new[] {
-                                           bmd.FullName
-                                       },
-                                       new List<string>(),
-                                       new List<string>(),
-                                       true);
+        var mapSubdir = subdir.TryToGetSubdir("map");
+        var bmdFile = mapSubdir.TryToGetSubdir("map")
+                               .Files.Single(file => file.Name == "map.bmd");
+        this.ExtractModels_(mapSubdir, new[] {bmdFile});
       }
+    }
+
+    private void ExtractModels_(
+        IFileHierarchyDirectory directory,
+        IReadOnlyList<IFileHierarchyFile> bmdFiles,
+        IReadOnlyList<IFile>? bcxFiles = null,
+        IReadOnlyList<IFile>? btiFiles = null,
+        bool allowMultipleAnimatedModels = false
+    ) {
+      Asserts.True(bmdFiles.Count > 0);
+
+      var outputDirectory =
+          GameFileHierarchyUtil.GetOutputDirectoryForDirectory(directory);
+
+      var matches = 0;
+      var existingModelFiles =
+          outputDirectory.GetExistingFiles()
+                         .Where(file => file.Extension == ".fbx" ||
+                                        file.Extension == ".glb")
+                         .ToArray();
+
+      foreach (var bmdFile in bmdFiles) {
+        if (existingModelFiles.Any(
+            existingModelFile => {
+              var existingName = existingModelFile.Name.Substring(
+                  0,
+                  existingModelFile.Name.Length -
+                  existingModelFile.Extension.Length);
+              var bmdName =
+                  bmdFile.Name.Substring(0,
+                                         bmdFile.Name.Length - ".mod".Length);
+
+              return bmdName == existingName ||
+                     bmdName + "_gltf" == existingName;
+            })) {
+          ++matches;
+        }
+      }
+
+      if (matches == bmdFiles.Count) {
+        this.logger_.LogInformation(
+            $"Model(s) already processed from {directory.LocalPath}");
+        return;
+      }
+
+      bcxFiles ??= new List<IFile>();
+      btiFiles ??= new List<IFile>();
+
+      MessageUtil.LogExtracting(this.logger_, directory, bmdFiles);
+
+      try {
+        new ManualBmd2FbxApi().Process(outputDirectory,
+                                       bmdFiles.Select(file => file.FullName)
+                                               .ToArray(),
+                                       bcxFiles.Select(file => file.FullName)
+                                               .ToArray(),
+                                       btiFiles.Select(file => file.FullName)
+                                               .ToArray(),
+                                       !allowMultipleAnimatedModels,
+                                       60);
+      } catch (Exception e) {
+        this.logger_.LogError(e.ToString());
+      }
+      this.logger_.LogInformation(" ");
     }
   }
 }
