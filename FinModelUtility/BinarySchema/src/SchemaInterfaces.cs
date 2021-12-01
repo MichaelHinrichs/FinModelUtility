73e48a2c46923e1612cf95d68768606f578df9c2
MeltyPlayer <mastergamepro@hotmail.com>
2021-11-30 20:51:27 -0600

    Set up interfaces/attributes for the binary schema analyzers/generators. This will need to be cleaned up later.

---
+++ FinModelUtility/BinarySchema/src/SchemaInterfaces.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace schema {
+  [AttributeUsage(AttributeTargets.Class)]
+  public class SchemaAttribute : Attribute {
+    public string MagicText { get; set; }
+  }
+
+
+  public enum IntType {
+    BYTE,
+    SBYTE,
+    SHORT,
+    USHORT,
+    INT,
+    UINT,
+    LONG,
+    ULONG
+  }
+
+
+  public interface ISchemaStringAttribute {}
+
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class NullTerminatedSchemaStringAttribute : Attribute,
+    ISchemaStringAttribute {}
+
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class SeparateLengthSchemaStringAttribute : Attribute,
+    ISchemaStringAttribute {
+    public SeparateLengthSchemaStringAttribute(IntType countType) {
+      this.CountType = countType;
+    }
+
+    public IntType CountType { get; }
+  }
+
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class SeparateLengthSchemaArrayAttribute : Attribute {
+    public SeparateLengthSchemaArrayAttribute(IntType countType) {
+      this.CountType = countType;
+    }
+
+    public IntType CountType { get; }
+  }
+
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class ConstLengthSchemaArrayAttribute : Attribute {
+    public ConstLengthSchemaArrayAttribute(int count) {
+      this.Count = count;
+    }
+
+    public int Count { get; }
+  }
+}
