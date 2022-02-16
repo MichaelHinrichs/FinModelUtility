d2dd4f85c42087fc704b53378ced7bde99d1ab88
MeltyPlayer <mastergamepro@hotmail.com>
2022-02-16 03:52:01 -0600

    Formatted HwXtdResource.

--- FinModelUtility/HaloWarsTools/Resources/HWXtdResource.cs
+++ FinModelUtility/HaloWarsTools/Resources/HWXtdResource.cs
@@ -11,19 +11,24 @@ using fin.model;
 using fin.model.impl;
 
 
-
 namespace HaloWarsTools {
   public class HWXtdResource : HWBinaryResource {
     public IModel Mesh => ValueCache.Get(ImportMesh);
 
     public Bitmap AmbientOcclusionTexture =>
-        ValueCache.Get(() => ExtractEmbeddedDXT5A(GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_AOChunk)));
+        ValueCache.Get(() => ExtractEmbeddedDXT5A(
+                           GetFirstChunkOfType(
+                               HWBinaryResourceChunkType.XTD_AOChunk)));
 
     public Bitmap OpacityTexture =>
-        ValueCache.Get(() => ExtractEmbeddedDXT5A(GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_AlphaChunk)));
+        ValueCache.Get(() => ExtractEmbeddedDXT5A(
+                           GetFirstChunkOfType(
+                               HWBinaryResourceChunkType.XTD_AlphaChunk)));
 
-    public static new HWXtdResource FromFile(HWContext context, string filename) {
-      return GetOrCreateFromFile(context, filename, HWResourceType.Xtd) as HWXtdResource;
+    public static new HWXtdResource
+        FromFile(HWContext context, string filename) {
+      return GetOrCreateFromFile(context, filename, HWResourceType.Xtd) as
+                 HWXtdResource;
     }
 
     private Bitmap ExtractEmbeddedDXT5A(HWBinaryResourceChunk chunk) {
@@ -40,8 +45,10 @@ namespace HaloWarsTools {
       var decompressedColorData = new byte[3 * width * height];
       DxtDecoder.DecompressDxt5a(data, width, height, decompressedColorData);
 
-      GCHandle m_bitsHandle = GCHandle.Alloc(decompressedColorData, GCHandleType.Pinned);
-      Bitmap bitmap = new Bitmap(width, height, 3 * width, PixelFormat.Format24bppRgb,
+      GCHandle m_bitsHandle =
+          GCHandle.Alloc(decompressedColorData, GCHandleType.Pinned);
+      Bitmap bitmap = new Bitmap(width, height, 3 * width,
+                                 PixelFormat.Format24bppRgb,
                                  m_bitsHandle.AddrOfPinnedObject());
       m_bitsHandle.Free();
 
@@ -52,9 +59,13 @@ namespace HaloWarsTools {
       int stride = 1;
       MeshNormalExportMode shadingMode = MeshNormalExportMode.Unchanged;
 
-      HWBinaryResourceChunk headerChunk = GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_XTDHeader);
-      float tileScale = BinaryUtils.ReadFloatBigEndian(RawBytes, (int) headerChunk.Offset + 12);
-      HWBinaryResourceChunk atlasChunk = GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_AtlasChunk);
+      HWBinaryResourceChunk headerChunk =
+          GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_XTDHeader);
+      float tileScale =
+          BinaryUtils.ReadFloatBigEndian(RawBytes,
+                                         (int) headerChunk.Offset + 12);
+      HWBinaryResourceChunk atlasChunk =
+          GetFirstChunkOfType(HWBinaryResourceChunkType.XTD_AtlasChunk);
 
       int gridSize =
           (int) Math.Round(Math.Sqrt((atlasChunk.Size - 32) /
@@ -68,9 +79,14 @@ namespace HaloWarsTools {
       }
 
       // These are stored as ZYX, 4 bytes per component
-      Vector3 PosCompMin = BinaryUtils.ReadVector3BigEndian(RawBytes, (int) atlasChunk.Offset).ReverseComponents();
+      Vector3 PosCompMin = BinaryUtils
+                           .ReadVector3BigEndian(
+                               RawBytes, (int) atlasChunk.Offset)
+                           .ReverseComponents();
       Vector3 PosCompRange =
-          BinaryUtils.ReadVector3BigEndian(RawBytes, (int) atlasChunk.Offset + 16).ReverseComponents();
+          BinaryUtils
+              .ReadVector3BigEndian(RawBytes, (int) atlasChunk.Offset + 16)
+              .ReverseComponents();
 
       var finModel = new ModelImpl();
       var finMesh = finModel.Skin.AddMesh();
@@ -80,22 +96,28 @@ namespace HaloWarsTools {
       // Read vertex offsets/normals and add them to the mesh
       for (int x = 0; x < gridSize; x += stride) {
         for (int z = 0; z < gridSize; z += stride) {
-          int index = ConvertGridPositionToIndex(new Tuple<int, int>(x, z), gridSize);
+          int index =
+              ConvertGridPositionToIndex(new Tuple<int, int>(x, z), gridSize);
           int offset = index * 4;
 
 
           // Get offset position and normal for this vertex
-          Vector3 position = ReadVector3Compressed(positionOffset + offset) * PosCompRange - PosCompMin;
+          Vector3 position =
+              ReadVector3Compressed(positionOffset + offset) * PosCompRange -
+              PosCompMin;
 
           // Positions are relative to the terrain grid, so shift them by the grid position
           position += new Vector3(x, 0, z) * tileScale;
 
           Vector3 normal =
               ConvertDirectionVector(
-                  Vector3.Normalize(ReadVector3Compressed(normalOffset + offset) * 2.0f - Vector3.One));
+                  Vector3.Normalize(
+                      ReadVector3Compressed(normalOffset + offset) * 2.0f -
+                      Vector3.One));
 
           // Simple UV based on original, non-warped terrain grid
-          Vector3 texCoord = new Vector3(x / ((float) gridSize - 1), z / ((float) gridSize - 1), 0);
+          Vector3 texCoord = new Vector3(x / ((float) gridSize - 1),
+                                         z / ((float) gridSize - 1), 0);
 
           var finVertex =
               finModel.Skin
@@ -131,14 +153,19 @@ namespace HaloWarsTools {
       => z * gridSize + x;
 
     private static int GetVertexID(int x, int z, int gridSize, int stride) {
-      return ConvertGridPositionToIndex(new Tuple<int, int>(x / stride, z / stride), gridSize / stride);
+      return ConvertGridPositionToIndex(
+          new Tuple<int, int>(x / stride, z / stride), gridSize / stride);
     }
 
-    private static Tuple<int, int> ConvertIndexToGridPosition(int index, int gridSize) {
+    private static Tuple<int, int> ConvertIndexToGridPosition(
+        int index,
+        int gridSize) {
       return new Tuple<int, int>(index % gridSize, index / gridSize);
     }
 
-    private static int ConvertGridPositionToIndex(Tuple<int, int> gridPosition, int gridSize) {
+    private static int ConvertGridPositionToIndex(
+        Tuple<int, int> gridPosition,
+        int gridSize) {
       return gridPosition.Item2 * gridSize + gridPosition.Item1;
     }
 
