cb9f61f8ff33f5ef9c96f1ec0faf63750de23902
MeltyPlayer <mastergamepro@hotmail.com>
2022-02-21 20:51:19 -0600

    Created a utility method for converting a Quaternion to Euler angles.

---
+++ FinModelUtility/Fin Tests/math/QuaternionUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using fin.util.asserts;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fin.math {
+  [TestClass]
+  public class QuaternionUtilTest {
+    [TestMethod]
+    public void ToEulerDegrees() {
+      var degToRad = MathF.PI / 180;
+
+      var x = 1 * 20 * degToRad;
+      var y = 2 * 20 * degToRad;
+      var z = 3 * 20 * degToRad;
+
+      var q = QuaternionUtil.Create(x, y, z);
+
+      var v = QuaternionUtil.ToEulerRadians(q);
+
+      Asserts.Equal(x, v.X);
+      Asserts.Equal(y, v.Y);
+      Asserts.Equal(z, v.Z);
+    }
+  }
+}
--- FinModelUtility/Fin/src/math/QuaternionUtil.cs
+++ FinModelUtility/Fin/src/math/QuaternionUtil.cs
@@ -1,6 +1,8 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 using fin.model;
+
 
 namespace fin.math {
   public static class QuaternionUtil {
@@ -18,6 +20,30 @@ namespace fin.math {
       var qx = Quaternion.CreateFromYawPitchRoll(0, xRadians, 0);
 
       return Quaternion.Normalize(qz * qy * qx);
+    }
+
+    public static Vector3 ToEulerRadians(Quaternion q) {
+      Vector3 angles = new();
+
+      // roll / x
+      double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
+      double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
+      angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
+
+      // pitch / y
+      double sinp = 2 * (q.W * q.Y - q.Z * q.X);
+      if (Math.Abs(sinp) >= 1) {
+        angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
+      } else {
+        angles.Y = (float)Math.Asin(sinp);
+      }
+
+      // yaw / z
+      double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
+      double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+      angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
+
+      return angles;
     }
   }
 }
