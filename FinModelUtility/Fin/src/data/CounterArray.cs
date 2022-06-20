1909fae975a83f4078c5ba573675c57091c8bb05
MeltyPlayer <mastergamepro@hotmail.com>
2022-06-20 00:47:53 -0500

    Set up a helper class for counting at different indices.

---
+++ FinModelUtility/Fin/src/data/CounterArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace fin.data {
+  public class CounterArray : IEnumerable<int> {
+    private readonly List<int> impl_ = new();
+
+    public int Count => this.impl_.Count;
+
+    public void Clear() => this.impl_.Clear();
+
+    public int Increment(int index) {
+      while (this.impl_.Count < index + 1) {
+        this.impl_.Add(0);
+      }
+
+      return this.impl_[index] += 1;
+    }
+
+    public int this[int index] => this.impl_[index];
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerator<int> GetEnumerator() => this.impl_.GetEnumerator();
+  }
+}
