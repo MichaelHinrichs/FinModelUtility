2a8626d4e88a2c8c4f74bbc1c57bbbacd6bc0806
MeltyPlayer <mastergamepro@hotmail.com>
2022-06-25 20:02:43 -0500

    Set up a new class for serializing to JSON.

---
+++ FinModelUtility/Fin/src/util/json/JsonUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+
+
+namespace fin.util.json {
+  public static class JsonUtil {
+    public static string Serialize(object obj) {
+      var jsonSerializer = new JsonSerializer {
+          Formatting = Formatting.Indented
+      };
+      var jsonTextWriter = new StringWriter();
+      jsonSerializer.Serialize(jsonTextWriter, obj);
+      return jsonTextWriter.ToString();
+    }
+  }
+}
