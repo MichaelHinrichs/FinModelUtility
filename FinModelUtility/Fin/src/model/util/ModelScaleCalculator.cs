806448c0481a9b088dd9f359f3ce3b3238b290d0
MeltyPlayer <mastergamepro@hotmail.com>
2022-07-05 22:21:59 -0500

    Added support for bone-only models to the model scale calculator.

--- FinModelUtility/Fin/src/model/util/ModelScaleCalculator.cs
+++ FinModelUtility/Fin/src/model/util/ModelScaleCalculator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using Assimp.Configs;
 
 using fin.math;
 using fin.model.impl;
@@ -33,9 +36,13 @@ namespace fin.model.util {
       var maxZ = float.MinValue;
 
       var position = new ModelImpl.PositionImpl();
+
+      var anyVertices = false;
       foreach (var mesh in model.Skin.Meshes) {
         foreach (var primitive in mesh.Primitives) {
           foreach (var vertex in primitive.Vertices) {
+            anyVertices = true;
+
             boneTransformManager.ProjectVertex(vertex, position);
 
             var x = position.X;
@@ -50,6 +57,34 @@ namespace fin.model.util {
 
             minZ = MathF.Min(minZ, z);
             maxZ = MathF.Max(maxZ, z);
+          }
+        }
+      }
+
+      if (!anyVertices) {
+        var boneQueue = new Queue<IBone>();
+        boneQueue.Enqueue(model.Skeleton.Root);
+
+        while (boneQueue.Count > 0) {
+          var bone = boneQueue.Dequeue();
+
+          var x = 0d;
+          var y = 0d;
+          var z = 0d;
+
+          boneTransformManager.ProjectVertex(bone, ref x, ref y, ref z);
+
+          minX = MathF.Min(minX, (float) x);
+          maxX = MathF.Max(maxX, (float) x);
+
+          minY = MathF.Min(minY, (float) y);
+          maxY = MathF.Max(maxY, (float) y);
+
+          minZ = MathF.Min(minZ, (float) z);
+          maxZ = MathF.Max(maxZ, (float) z);
+
+          foreach (var child in bone.Children) {
+            boneQueue.Enqueue(child);
           }
         }
       }
