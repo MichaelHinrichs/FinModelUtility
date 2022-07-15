48d1a391c3598731d42ccca1926d85453216a2ae
MeltyPlayer <mastergamepro@hotmail.com>
2022-07-15 16:24:26 -0500

    Started looking into adding support for Super Smash Bros. Melee models (.dat).

---
+++ FinModelUtility/Dat/src/schema/Dat.cs
@@ -0,0 +1,34 @@
+﻿using fin.util.asserts;
+
+using schema;
+
+
+namespace dat.schema {
+  public class Dat : IDeserializable {
+    public void Read(EndianBinaryReader er) {
+      var fileHeaderLength = 0x20;
+      var fileHeader = er.ReadNew<FileHeader>();
+      Asserts.Equal(fileHeaderLength, er.Position);
+
+      // Reads pointer table
+      er.Position = fileHeaderLength + fileHeader.PointerTableOffset;
+      er.ReadUInt32s((int) fileHeader.PointerCount);
+    }
+  }
+
+  [Schema]
+  public partial class FileHeader : IBiSerializable {
+    public uint FileSize { get; set; }
+
+    public uint PointerTableOffset { get; set; }
+    public uint PointerCount { get; set; }
+    public uint RootNodeCount { get; set; }
+    public uint ReferenceNodeCount { get; set; }
+
+    [StringLengthSource(4)]
+    public string Unknown0 { get; set; }
+    public uint Unknown1 { get; set; }
+    public uint Unknown2 { get; set; }
+
+  }
+}
---
+++ FinModelUtility/UniversalModelExtractor/src/games/super_smash_bros_melee/SuperSmashBrosMeleeModelFileGatherer.cs
@@ -0,0 +1,30 @@
+﻿using bmd.exporter;
+
+using fin.model;
+
+using uni.platforms;
+using uni.platforms.gcn;
+
+
+namespace uni.games.super_smash_bros_melee {
+  public class
+      SuperSmashBrosMeleeModelFileGatherer : IModelFileGatherer<
+          BmdModelFileBundle> {
+    public IModelDirectory<BmdModelFileBundle>? GatherModelFileBundles(
+        bool assert) {
+      var superSmashBrosMeleeRom =
+          DirectoryConstants.ROMS_DIRECTORY.TryToGetExistingFile(
+              "super_smash_bros_melee.gcm");
+      if (superSmashBrosMeleeRom == null) {
+        return null;
+      }
+
+      var options = GcnFileHierarchyExtractor.Options.Standard();
+      var fileHierarchy =
+          new GcnFileHierarchyExtractor()
+              .ExtractFromRom(options, superSmashBrosMeleeRom);
+
+      return null;
+    }
+  }
+}
