0b04ecf42f280ada7811e142fe40cdaaf0400395
MeltyPlayer <mastergamepro@hotmail.com>
2022-07-15 16:26:21 -0500

    Started looking into adding support for Majora's Mask 3D.

--- FinModelUtility/UniversalModelExtractor/src/games/RootFileGatherer.cs
+++ FinModelUtility/UniversalModelExtractor/src/games/RootFileGatherer.cs
@@ -3,11 +3,13 @@
 using uni.games.battalion_wars;
 using uni.games.glover;
 using uni.games.luigis_mansion_3d;
+using uni.games.majoras_mask_3d;
 using uni.games.mario_kart_double_dash;
 using uni.games.ocarina_of_time_3d;
 using uni.games.pikmin_1;
 using uni.games.pikmin_2;
 using uni.games.super_mario_sunshine;
+using uni.games.super_smash_bros_melee;
 
 
 namespace uni.games {
@@ -19,11 +21,13 @@ namespace uni.games {
           new BattalionWarsFileGatherer(),
           new GloverModelFileGatherer(),
           new LuigisMansion3dModelFileGatherer(),
+          new MajorasMask3dFileGatherer(),
           new MarioKartDoubleDashFileGatherer(),
           new OcarinaOfTime3dFileGatherer(),
           new Pikmin1ModelFileGatherer(),
           new Pikmin2FileGatherer(),
           new SuperMarioSunshineModelFileGatherer(),
+          new SuperSmashBrosMeleeModelFileGatherer(),
       };
 
       foreach (var gatherer in gatherers) {
---
+++ FinModelUtility/UniversalModelExtractor/src/games/majoras_mask_3d/MajorasMask3dFileGatherer.cs
@@ -0,0 +1,65 @@
+﻿using fin.model;
+
+using uni.platforms;
+using uni.platforms.threeDs;
+using uni.util.io;
+using uni.util.separator;
+
+using cmb.api;
+
+
+namespace uni.games.majoras_mask_3d {
+  public class MajorasMask3dFileGatherer : IModelFileGatherer<
+      CmbModelFileBundle> {
+    private readonly IModelSeparator separator_
+        = new ModelSeparator(directory => directory.Name);
+
+
+    public IModelDirectory<CmbModelFileBundle>? GatherModelFileBundles(
+        bool assert) {
+      var majorasMask3dRom =
+          DirectoryConstants.ROMS_DIRECTORY.TryToGetExistingFile(
+              "majoras_mask_3d.cia");
+      if (majorasMask3dRom == null) {
+        return null;
+      }
+
+      var fileHierarchy =
+          new ThreeDsFileHierarchyExtractor()
+              .ExtractFromRom(majorasMask3dRom);
+
+      return new FileHierarchyBundler<CmbModelFileBundle>(
+          subdir => {
+            if (!separator_.Contains(subdir)) {
+              return null;
+            }
+
+            var cmbFiles =
+                subdir.FilesWithExtensionsRecursive(".cmb").ToArray();
+            if (cmbFiles.Length == 0) {
+              return null;
+            }
+
+            var csabFiles =
+                subdir.FilesWithExtensionsRecursive(".csab").ToArray();
+            var ctxbFiles =
+                subdir.FilesWithExtensionsRecursive(".ctxb").ToArray();
+
+            try {
+              var bundles =
+                  this.separator_.Separate(subdir, cmbFiles, csabFiles);
+
+              return bundles.Select(bundle => new CmbModelFileBundle(
+                                        bundle.ModelFile,
+                                        bundle.AnimationFiles.ToArray(),
+                                        ctxbFiles,
+                                        null
+                                    ));
+            } catch {
+              return null;
+            }
+          }
+      ).GatherBundles(fileHierarchy);
+    }
+  }
+}
