4ab86c7c36ac8b334db1d50402986bc62f30fd67
MeltyPlayer <mastergamepro@hotmail.com>
2022-07-26 21:46:29 -0500

    Added some documentation comments to some schema types.

--- FinModelUtility/BinarySchema/src/attributes/ignore/IgnoreAttribute.cs
+++ FinModelUtility/BinarySchema/src/attributes/ignore/IgnoreAttribute.cs
@@ -2,6 +2,9 @@
 
 
 namespace schema.attributes.ignore {
+  /// <summary>
+  ///   Schema attribute for designating a member that should not be included when reading/writing.
+  /// </summary>
   [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
   public class IgnoreAttribute : Attribute { }
 }
