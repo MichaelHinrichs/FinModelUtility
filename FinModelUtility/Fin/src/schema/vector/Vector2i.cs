3c0fefe7c3110eda2b0e40ec79d31a274bb6a9b3
MeltyPlayer <mastergamepro@hotmail.com>
2022-08-10 19:47:29 -0500

    Moved Mod's Vector2i and Vector3i classes into Fin.

---
+++ FinModelUtility/Fin/src/schema/vector/Vector2i.cs
@@ -0,0 +1,10 @@
+﻿using schema;
+
+
+namespace fin.schema.vector {
+  [Schema]
+  public partial class Vector2i : IBiSerializable {
+    public int X { get; set; }
+    public int Y { get; set; }
+  }
+}
---
+++ FinModelUtility/Fin/src/schema/vector/Vector3i.cs
@@ -0,0 +1,17 @@
+﻿using schema;
+
+
+namespace fin.schema.vector {
+  [Schema]
+  public partial class Vector3i : IBiSerializable {
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    public void Set(int x, int y, int z) {
+      this.X = x;
+      this.Y = y;
+      this.Z = z;
+    }
+  }
+}
