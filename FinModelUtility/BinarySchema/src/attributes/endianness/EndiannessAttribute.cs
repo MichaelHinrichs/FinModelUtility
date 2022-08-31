04aa6f09b781ec9fbff82d3c888671d19f05fc8c
MeltyPlayer <mastergamepro@hotmail.com>
2022-08-31 15:28:25 -0500

    Added an unimplemented attribute for setting the Endianness on a class/field.

---
+++ FinModelUtility/BinarySchema/src/attributes/endianness/EndiannessAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+
+namespace schema.attributes.align {
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct |
+                  AttributeTargets.Field | AttributeTargets.Property)]
+  public class EndiannessAttribute : Attribute {
+    public EndiannessAttribute(Endianness endianness) {
+      this.Endianness = endianness;
+    }
+
+    public Endianness Endianness { get; }
+  }
+}
