791eac564ad4494e7115cc838d72f0f147d3c059
MeltyPlayer <mastergamepro@hotmail.com>
2022-09-01 17:06:58 -0500

    Updated the enum test to reflect that enums without formats are supported.

--- FinModelUtility/BinarySchema Tests/SchemaStructureParserEnumTest.cs
+++ FinModelUtility/BinarySchema Tests/SchemaStructureParserEnumTest.cs
@@ -2,9 +2,30 @@ using Microsoft.CodeAnalysis;
 
 using NUnit.Framework;
 
+
 namespace schema {
   public partial class SchemaStructureParserTest {
     public class Enum {
+      [Test]
+      public void TestEnumWithFormat() {
+        var structure = SchemaTestUtil.Parse(@"
+namespace foo.bar {
+  public enum ValueType : byte {
+    A,
+    B,
+    C
+  }
+
+  [BinarySchema]
+  public partial class EnumWrapper {
+    public ValueType field;
+  }
+}");
+        SchemaTestUtil.AssertDiagnostics(
+            structure.Diagnostics,
+            System.Array.Empty<DiagnosticDescriptor>());
+      }
+
       [Test]
       public void TestEnumWithoutFormat() {
         var structure = SchemaTestUtil.Parse(@"
@@ -20,7 +41,9 @@ namespace foo.bar {
     public ValueType field;
   }
 }");
-        SchemaTestUtil.AssertDiagnostics(structure.Diagnostics, Rules.EnumNeedsIntegerFormat);
+        SchemaTestUtil.AssertDiagnostics(
+            structure.Diagnostics,
+            System.Array.Empty<DiagnosticDescriptor>());
       }
 
       [Test]
@@ -38,7 +61,9 @@ namespace foo.bar {
     public readonly ValueType[] field = new ValueType[1];
   }
 }");
-        SchemaTestUtil.AssertDiagnostics(structure.Diagnostics, Rules.EnumNeedsIntegerFormat);
+        SchemaTestUtil.AssertDiagnostics(
+            structure.Diagnostics,
+            System.Array.Empty<DiagnosticDescriptor>());
       }
     }
   }
