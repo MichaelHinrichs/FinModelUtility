3eb0f2279484f81defe1efccba50ca8a4491e885
MeltyPlayer <mastergamepro@hotmail.com>
2022-09-01 23:21:03 -0500

    Added documentation to EndianAttribute.

--- FinModelUtility/BinarySchema/src/attributes/endianness/EndiannessAttribute.cs
+++ FinModelUtility/BinarySchema/src/attributes/endianness/EndiannessAttribute.cs
@@ -3,6 +3,19 @@ using System.IO;
 
 
 namespace schema.attributes.align {
+  /// <summary>
+  ///   Attribute for specifying the endianness of a given class or field.
+  ///
+  ///   <para>
+  ///     When added to a class, this will be used unless a different
+  ///     endianness was already specified for the field, for the overall
+  ///     reader/writer.
+  ///   </para>
+  ///   <para>
+  ///     When added to a field, this take precedence over any preexisting
+  ///     field, reader/writer, or class endianness.
+  ///   </para>
+  /// </summary>
   [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct |
                   AttributeTargets.Field | AttributeTargets.Property)]
   public class EndiannessAttribute : Attribute {
