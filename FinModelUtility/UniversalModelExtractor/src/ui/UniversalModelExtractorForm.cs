6fbf0c30b14bf68e543cbcd40f01b8aa1e8246ef
MeltyPlayer <mastergamepro@hotmail.com>
2022-09-06 19:49:38 -0500

    Switched UniversalModelExtractorForm to inline namespace.

--- FinModelUtility/UniversalModelExtractor/src/ui/UniversalModelExtractorForm.cs
+++ FinModelUtility/UniversalModelExtractor/src/ui/UniversalModelExtractorForm.cs
@@ -9,46 +9,47 @@ using fin.util.asserts;
 using uni.ui.common;
 
 
-namespace uni.ui {
-  public partial class UniversalModelExtractorForm : Form {
-    public UniversalModelExtractorForm() {
-      InitializeComponent();
+namespace uni.ui; 
 
-      this.modelViewerGlPanel_.AnimationPlaybackManager =
-          this.modelTabs_.AnimationPlaybackManager;
-      this.modelTabs_.OnAnimationSelected += animation =>
-          this.modelViewerGlPanel_.Animation = animation;
-    }
+public partial class UniversalModelExtractorForm : Form {
+  public UniversalModelExtractorForm() {
+    this.InitializeComponent();
 
-    private void UniversalModelExtractorForm_Load(object sender, EventArgs e) {
-      this.modelFileTreeView_.Populate(
-          new RootModelFileGatherer().GatherAllModelFiles());
+    this.modelViewerGlPanel_.AnimationPlaybackManager =
+        this.modelTabs_.AnimationPlaybackManager;
+    this.modelTabs_.OnAnimationSelected += animation =>
+        this.modelViewerGlPanel_.Animation = animation;
+  }
 
-      this.modelFileTreeView_.DirectorySelected += this.OnDirectorySelect_;
-      this.modelFileTreeView_.FileSelected += this.OnFileSelect_;
-    }
+  private void UniversalModelExtractorForm_Load(object sender, EventArgs e) {
+    this.modelFileTreeView_.Populate(
+        new RootModelFileGatherer().GatherAllModelFiles());
 
-    private void OnDirectorySelect_(
-        IFileTreeNode<IModelFileBundle> directoryNode) {
-      this.modelToolStrip_.DirectoryNode = directoryNode;
-    }
+    this.modelFileTreeView_.DirectorySelected += this.OnDirectorySelect_;
+    this.modelFileTreeView_.FileSelected += this.OnFileSelect_;
+  }
 
-    private void OnFileSelect_(IFileTreeNode<IModelFileBundle> fileNode) {
-      var model = this.LoadModel_(Asserts.CastNonnull(fileNode.File));
-      this.modelToolStrip_.DirectoryNode = fileNode.Parent;
-      this.modelToolStrip_.FileNodeAndModel = (fileNode, model);
-      this.modelViewerGlPanel_.Model = model;
-      this.modelTabs_.Model = model;
-    }
+  private void OnDirectorySelect_(
+      IFileTreeNode<IModelFileBundle> directoryNode) {
+    this.modelToolStrip_.DirectoryNode = directoryNode;
+  }
 
-    private IModel LoadModel_(IModelFileBundle modelFileBundle)
-      => new GlobalModelLoader().LoadModel(modelFileBundle);
+  private void OnFileSelect_(IFileTreeNode<IModelFileBundle> fileNode) {
+    var model = this.LoadModel_(Asserts.CastNonnull(fileNode.File));
+    this.modelToolStrip_.DirectoryNode = fileNode.Parent;
+    this.modelToolStrip_.FileNodeAndModel = (fileNode, model);
+    this.modelViewerGlPanel_.Model = model;
+    this.modelTabs_.Model = model;
+  }
 
-    private void modelViewerGlPanel__Load(object sender, EventArgs e) { }
+  private IModel LoadModel_(IModelFileBundle modelFileBundle) {
+    return new GlobalModelLoader().LoadModel(modelFileBundle);
+  }
 
-    private void gitHubToolStripMenuItem_Click(object sender, EventArgs e) {
-      Process.Start("explorer",
-                    "https://github.com/MeltyPlayer/FinModelUtility");
-    }
+  private void modelViewerGlPanel__Load(object sender, EventArgs e) { }
+
+  private void gitHubToolStripMenuItem_Click(object sender, EventArgs e) {
+    Process.Start("explorer",
+                  "https://github.com/MeltyPlayer/FinModelUtility");
   }
 }
