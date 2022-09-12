b1be6bd045e9664e61034cda6a4349a7fdd1d225
MeltyPlayer <mastergamepro@hotmail.com>
2022-09-12 16:33:53 -0500

    Added test coverage for LazyDictionary.

---
+++ FinModelUtility/Fin Tests/data/LazyDictionaryTest.cs
@@ -0,0 +1,108 @@
+﻿using fin.util.strings;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace fin.data {
+  [TestClass]
+  public class LazyDictionaryTest {
+    [TestMethod]
+    public void TestWithKeyAndValueHandler() {
+      var invokeCount = 0;
+      var lazyReverseMap = new LazyDictionary<string, string>(inStr => {
+        invokeCount++;
+        return inStr.Reverse();
+      });
+
+      Assert.AreEqual(0, lazyReverseMap.Count);
+      Assert.AreEqual(0, invokeCount);
+
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(1, invokeCount);
+
+      // Reuses existing value
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(1, invokeCount);
+    }
+
+    [TestMethod]
+    public void TestWithDictionaryKeyAndValueHandler() {
+      var invokeCount = 0;
+      LazyDictionary<string, string>? lazyReverseMap = null;
+      lazyReverseMap = new LazyDictionary<string, string>((dict, inStr) => {
+        Assert.AreSame(lazyReverseMap, dict);
+        invokeCount++;
+        return inStr.Reverse();
+      });
+
+      Assert.AreEqual(0, lazyReverseMap.Count);
+      Assert.AreEqual(0, invokeCount);
+
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(1, invokeCount);
+
+      // Reuses existing value
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(1, invokeCount);
+    }
+
+    [TestMethod]
+    public void TestSettingValuesDirectly() {
+      var invokeCount = 0;
+      var lazyReverseMap = new LazyDictionary<string, string>(inStr => {
+        invokeCount++;
+        return inStr.Reverse();
+      });
+
+      Assert.AreEqual(0, lazyReverseMap.Count);
+      Assert.AreEqual(0, invokeCount);
+
+      lazyReverseMap["reverse"] = "esrever";
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(0, invokeCount);
+    }
+
+    [TestMethod]
+    public void TestClear() {
+      var invokeCount = 0;
+      var lazyReverseMap = new LazyDictionary<string, string>(inStr => {
+        invokeCount++;
+        return inStr.Reverse();
+      });
+
+      Assert.AreEqual(0, lazyReverseMap.Count);
+      Assert.AreEqual(0, invokeCount);
+
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(1, invokeCount);
+
+      lazyReverseMap.Clear();
+      Assert.AreEqual(0, lazyReverseMap.Count);
+      Assert.AreEqual(1, invokeCount);
+
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(1, lazyReverseMap.Count);
+      Assert.AreEqual(2, invokeCount);
+    }
+
+    [TestMethod]
+    public void TestContainsKey() {
+      var lazyReverseMap =
+          new LazyDictionary<string, string>(inStr => inStr.Reverse());
+
+      Assert.AreEqual(false, lazyReverseMap.ContainsKey("reverse"));
+
+      Assert.AreEqual("esrever", lazyReverseMap["reverse"]);
+      Assert.AreEqual(true, lazyReverseMap.ContainsKey("reverse"));
+
+      lazyReverseMap.Clear();
+      Assert.AreEqual(false, lazyReverseMap.ContainsKey("reverse"));
+    }
+  }
+}
