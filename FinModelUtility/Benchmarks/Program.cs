644cfebbdaf6ad29d915c468cb7d70740d781630
MeltyPlayer <mastergamepro@hotmail.com>
2022-09-21 17:03:07 -0500

    Set up a benchmarking project for testing speed.

---
+++ FinModelUtility/Benchmarks/Program.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace benchmarks {
+  public class FetchDirectories {
+    [Benchmark]
+    public void FetchDirectoriesSeparately() =>
+        FetchDirectoriesSeparatelyImpl().ToArray();
+
+    [Benchmark]
+    public void FetchDirectoriesAllAtOnce() =>
+        FetchDirectoriesAllAtOnceImpl();
+
+    public const string DIRECTORY =
+        @"R:\Documents\CSharpWorkspace\Pikmin2Utility\cli\roms";
+
+    public const string SEARCH_PATTERN = "*";
+
+    public IEnumerable<string> FetchDirectoriesSeparatelyImpl() {
+      var rootFolderPath = DIRECTORY;
+      var pending = new Queue<string>();
+      pending.Enqueue(rootFolderPath);
+      string[] tmp;
+      while (pending.Count > 0) {
+        rootFolderPath = pending.Dequeue();
+        try {
+          tmp = Directory.GetFiles(rootFolderPath, SEARCH_PATTERN);
+        } catch (UnauthorizedAccessException) {
+          continue;
+        }
+        for (int i = 0; i < tmp.Length; i++) {
+          yield return tmp[i];
+        }
+        tmp = Directory.GetDirectories(rootFolderPath);
+        for (int i = 0; i < tmp.Length; i++) {
+          pending.Enqueue(tmp[i]);
+        }
+      }
+    }
+
+    public string[] FetchDirectoriesAllAtOnceImpl() {
+      return Directory.GetFiles(DIRECTORY, SEARCH_PATTERN,
+                                new EnumerationOptions {
+                                    RecurseSubdirectories =
+                                        true
+                                });
+    }
+  }
+
+  public class Program {
+    public static void Main(string[] args) {
+      var summary = BenchmarkRunner.Run<FetchDirectories>();
+    }
+  }
+}
