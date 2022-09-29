ca52725dcc1de0b2bdd3005b5691e75a7d6bcc31
MeltyPlayer <mastergamepro@hotmail.com>
2022-09-28 19:36:19 -0500

    Added more tests for matrices.

--- FinModelUtility/Fin Tests/math/MatrixTests.cs
+++ FinModelUtility/Fin Tests/math/MatrixTests.cs
@@ -7,6 +7,104 @@ namespace fin.math {
   [TestClass]
   public class MatrixTests {
     [TestMethod]
+    public void TestMultiplyByMatrix() {
+      var lhs = new FinMatrix4x4();
+      lhs[0, 0] = 1;
+      lhs[0, 1] = 2;
+      lhs[0, 2] = 3;
+      lhs[0, 3] = 4;
+      lhs[1, 0] = 5;
+      lhs[1, 1] = 6;
+      lhs[1, 2] = 7;
+      lhs[1, 3] = 8;
+      lhs[2, 0] = 9;
+      lhs[2, 1] = 10;
+      lhs[2, 2] = 11;
+      lhs[2, 3] = 12;
+      lhs[3, 0] = 13;
+      lhs[3, 1] = 14;
+      lhs[3, 2] = 15;
+      lhs[3, 3] = 16;
+
+      var rhs = new FinMatrix4x4();
+      rhs[0, 0] = 4;
+      rhs[0, 1] = 5;
+      rhs[0, 2] = 6;
+      rhs[0, 3] = 2;
+      rhs[1, 0] = 5;
+      rhs[1, 1] = 8;
+      rhs[1, 2] = 9;
+      rhs[1, 3] = 3;
+      rhs[2, 0] = 1;
+      rhs[2, 1] = 3;
+      rhs[2, 2] = 4;
+      rhs[2, 3] = 6;
+      rhs[3, 0] = 7;
+      rhs[3, 1] = 8;
+      rhs[3, 2] = 9;
+      rhs[3, 3] = 5;
+
+      var product = lhs.CloneAndMultiply(rhs);
+
+      Assert.AreEqual(45, product[0, 0]);
+      Assert.AreEqual(62, product[0, 1]);
+      Assert.AreEqual(72, product[0, 2]);
+      Assert.AreEqual(46, product[0, 3]);
+      Assert.AreEqual(113, product[1, 0]);
+      Assert.AreEqual(158, product[1, 1]);
+      Assert.AreEqual(184, product[1, 2]);
+      Assert.AreEqual(110, product[1, 3]);
+      Assert.AreEqual(181, product[2, 0]);
+      Assert.AreEqual(254, product[2, 1]);
+      Assert.AreEqual(296, product[2, 2]);
+      Assert.AreEqual(174, product[2, 3]);
+      Assert.AreEqual(249, product[3, 0]);
+      Assert.AreEqual(350, product[3, 1]);
+      Assert.AreEqual(408, product[3, 2]);
+      Assert.AreEqual(238, product[3, 3]);
+    }
+
+    [TestMethod]
+    public void TestMultiplyByScalar() {
+      var inputMatrix = new FinMatrix4x4();
+      inputMatrix[0, 0] = 1;
+      inputMatrix[0, 1] = 2;
+      inputMatrix[0, 2] = 3;
+      inputMatrix[0, 3] = 4;
+      inputMatrix[1, 0] = 5;
+      inputMatrix[1, 1] = 6;
+      inputMatrix[1, 2] = 7;
+      inputMatrix[1, 3] = 8;
+      inputMatrix[2, 0] = 9;
+      inputMatrix[2, 1] = 10;
+      inputMatrix[2, 2] = 11;
+      inputMatrix[2, 3] = 12;
+      inputMatrix[3, 0] = 13;
+      inputMatrix[3, 1] = 14;
+      inputMatrix[3, 2] = 15;
+      inputMatrix[3, 3] = 16;
+
+      inputMatrix.MultiplyInPlace(2);
+
+      Assert.AreEqual(2, inputMatrix[0, 0]);
+      Assert.AreEqual(4, inputMatrix[0, 1]);
+      Assert.AreEqual(6, inputMatrix[0, 2]);
+      Assert.AreEqual(8, inputMatrix[0, 3]);
+      Assert.AreEqual(10, inputMatrix[1, 0]);
+      Assert.AreEqual(12, inputMatrix[1, 1]);
+      Assert.AreEqual(14, inputMatrix[1, 2]);
+      Assert.AreEqual(16, inputMatrix[1, 3]);
+      Assert.AreEqual(18, inputMatrix[2, 0]);
+      Assert.AreEqual(20, inputMatrix[2, 1]);
+      Assert.AreEqual(22, inputMatrix[2, 2]);
+      Assert.AreEqual(24, inputMatrix[2, 3]);
+      Assert.AreEqual(26, inputMatrix[3, 0]);
+      Assert.AreEqual(28, inputMatrix[3, 1]);
+      Assert.AreEqual(30, inputMatrix[3, 2]);
+      Assert.AreEqual(32, inputMatrix[3, 3]);
+    }
+
+    [TestMethod]
     public void TestInvert() {
       var inputMatrix = new FinMatrix4x4();
       inputMatrix[0, 0] = 2;
@@ -119,7 +217,7 @@ namespace fin.math {
     private IReadOnlyFinMatrix4x4 GetCloseToIdentityMatrix_() {
       var closeToIdentityMatrix = new FinMatrix4x4().SetZero();
 
-      var error = .00001;
+      var error = .00001f;
       closeToIdentityMatrix[0, 0] = 1 + error;
       closeToIdentityMatrix[0, 1] = error;
       closeToIdentityMatrix[0, 2] = -error;
@@ -143,7 +241,7 @@ namespace fin.math {
     private IReadOnlyFinMatrix4x4 GetDifferentFromIdentityMatrix_() {
       var closeToIdentityMatrix = new FinMatrix4x4().SetZero();
 
-      var error = .001;
+      var error = .001f;
       closeToIdentityMatrix[0, 0] = 1 + error;
       closeToIdentityMatrix[0, 1] = error;
       closeToIdentityMatrix[0, 2] = -error;
