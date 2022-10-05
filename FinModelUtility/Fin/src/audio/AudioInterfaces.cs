b20dcc243f1af534a9d5aa519784a52e12d6a522
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-05 17:56:58 -0500

    Filled out the audio interfaces more.

--- FinModelUtility/Fin/src/audio/AudioInterfaces.cs
+++ FinModelUtility/Fin/src/audio/AudioInterfaces.cs
@@ -1,4 +1,6 @@
 ﻿using fin.model;
+using System;
+using System.Numerics;
 
 
 namespace fin.audio {
@@ -20,16 +22,24 @@ namespace fin.audio {
   }
 
 
+  public interface IAudioFormat<out TNumber> where TNumber : INumber<TNumber> {
+    AudioChannelsType AudioChannelsType { get; }
+    int Frequency { get; }
+    long Length { get; }
+  }
+
+  public interface IAudioData<out TNumber> : IAudioFormat<TNumber>
+      where TNumber : INumber<TNumber> {
+    TNumber GetPcm(AudioChannelType channelType, long index);
+  }
+
+
   /// <summary>
   ///   Type for storing static audio data, e.g. a loaded audio file.
   /// </summary>
-  public interface IAudioBuffer {
+  public interface IAudioBuffer<out TNumber> : IAudioData<TNumber>, IDisposable
+      where TNumber : INumber<TNumber> {
     string Name { get; }
-    AudioChannelsType AudioChannelsType { get; }
-    int Frequency { get; }
-
-    byte GetPcm(AudioChannelType channelType, long index); 
-    long Length { get; }
   }
 
   /// <summary>
@@ -37,35 +47,62 @@ namespace fin.audio {
   ///   streams to apply effects, or played out to the speakers via an audio
   ///   source.
   /// </summary>
-  public interface IAudioStream { }
+  public interface IAudioStream<out TNumber>
+      : IAudioData<TNumber> where TNumber : INumber<TNumber> { }
 
-  public interface IBufferAudioStream : IAudioStream {
-    IAudioBuffer InputBuffer { get; set; }
-    long Position { get; set; }
-
-    bool Looping { get; set; }
+  public interface IBufferAudioStream<TNumber> : IAudioStream<TNumber>
+      where TNumber : INumber<TNumber> {
+    IAudioBuffer<TNumber> InputBuffer { get; set; }
     bool Reversed { get; set; }
   }
 
-  public interface IEffectAudioStream : IAudioStream {
-    IAudioStream InputStream { get; set; }
+  public interface IEffectAudioStream<TNumber> : IAudioStream<TNumber>
+      where TNumber : INumber<TNumber> {
+    IAudioStream<TNumber> InputStream { get; set; }
   }
 
-  public interface IVolumeEffectAudioStream : IEffectAudioStream {
+  public interface IVolumeEffectAudioStream<TNumber>
+      : IEffectAudioStream<TNumber> where TNumber : INumber<TNumber> {
     float Volume { get; set; }
   }
 
-  public interface ISpeedEffectAudioStream : IEffectAudioStream {
+  public interface ISpeedEffectAudioStream<TNumber>
+      : IEffectAudioStream<TNumber> where TNumber : INumber<TNumber> {
     float Speed { get; set; }
   }
 
-  public interface IDopplerEffectAudioStream : IEffectAudioStream {
+  public interface IDopplerEffectAudioStream<TNumber>
+      : IEffectAudioStream<TNumber> where TNumber : INumber<TNumber> {
     IVector3 RelativePosition { get; set; }
     IVector3 RelativeVelocity { get; set; }
   }
 
 
-  public interface IAudioSource {
-    void PlayStream(IAudioStream stream);
+  public enum OutputState {
+    UNDEFINED,
+    STOPPED,
+    PLAYING,
+    PAUSED,
+    DISPOSED,
+  }
+
+
+  /// <summary>
+  ///   Type that plays audio data out through the speakers.
+  /// </summary>
+  public interface IAudioOutput<TNumber> : IAudioFormat<TNumber>, IDisposable
+      where TNumber : INumber<TNumber> {
+    IAudioStream<TNumber> Stream { get; }
+
+    OutputState State { get; }
+    void Play();
+    void Stop();
+    void Pause();
+
+
+    long Index { get; set; }
+    TNumber GetPcm(AudioChannelType channelType);
+
+    bool Looping { get; set; }
   }
 }
