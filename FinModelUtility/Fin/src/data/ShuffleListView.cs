a033305a7227893c5089730d7ec0cec8426e3b50
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-12 16:09:08 -0500

    Set up a helper class for getting shuffled values from a list.

---
+++ FinModelUtility/Fin/src/data/ShuffleListView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace fin.data {
+  public class ShuffledListView<T> {
+    private readonly IReadOnlyList<T> impl_;
+
+    public ShuffledListView(IReadOnlyList<T> impl) {
+      this.impl_ = impl;
+    }
+
+    // TODO: Implement an algorithm that "feels more random"
+    public T Next() => this.impl_[Random.Shared.Next(this.impl_.Count)];
+  }
+}
