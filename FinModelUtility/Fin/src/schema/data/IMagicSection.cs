0667f7d863e5200d88ba67c462c2cd0e16963bc4
MeltyPlayer <mastergamepro@hotmail.com>
2022-11-27 22:20:39 -0600

    Pulled out the logic for reading magic sections so that sized sections could be read separately.

--- FinModelUtility/Fin/src/schema/data/IMagicSection.cs
+++ FinModelUtility/Fin/src/schema/data/IMagicSection.cs
@@ -1,17 +1,11 @@
-﻿using System.IO;
-using System.Threading.Tasks;
-
-using fin.util.asserts;
-
-using schema;
+﻿using schema;
 using schema.attributes.ignore;
 
 
 namespace fin.schema.data {
-  public interface IMagicSection<T> : IBiSerializable
+  public interface IMagicSection<T> : ISizedSection<T>
       where T : IBiSerializable {
     string Magic { get; }
-    T Data { get; }
   }
 
   /// <summary>
@@ -25,60 +19,32 @@ namespace fin.schema.data {
       where T : IBiSerializable, new() {
     private readonly PassThruMagicUint32SizedSection<T> impl_;
 
+    public AutoMagicUInt32SizedSection(string magic) {
+      this.impl_ = new(magic, new T());
+    }
+
     [Ignore]
     public string Magic => this.impl_.Magic;
 
     [Ignore]
     public T Data => this.impl_.Data;
-
-    public AutoMagicUInt32SizedSection(string magic) {
-      this.impl_ = new(magic, new T());
-    }
   }
 
-  /// <summary>
-  ///   Type for 
-  /// </summary>
-  public class PassThruMagicUint32SizedSection<T> : IMagicSection<T>
+  [BinarySchema]
+  public partial class PassThruMagicUint32SizedSection<T> : IMagicSection<T>
       where T : IBiSerializable {
     public string Magic { get; }
-    public T Data { get; }
+
+    private readonly PassThruUint32SizedSection<T> impl_;
 
     public PassThruMagicUint32SizedSection(
         string magic,
         T data) {
       this.Magic = magic;
-      this.Data = data;
+      this.impl_ = new PassThruUint32SizedSection<T>(data);
     }
 
-    public void Read(EndianBinaryReader er) {
-      er.AssertString(this.Magic);
-
-      var expectedDataSize = er.ReadUInt32();
-
-      var dataStart = er.Position;
-      this.Data.Read(er);
-      var actualDataSize = er.Position - dataStart;
-      if (expectedDataSize != actualDataSize) {
-        Asserts.Fail(
-            $"Expected to read {expectedDataSize} bytes in section '{this.Magic}', but actually read {actualDataSize} bytes.");
-      }
-    }
-
-    public void Write(EndianBinaryWriter ew) {
-      ew.WriteString(this.Magic);
-
-      var beforeLengthTask = new TaskCompletionSource<long>();
-      ew.WriteUInt32Delayed(
-          beforeLengthTask.Task.ContinueWith(
-              length => (uint) length.Result));
-
-      var actualLengthTask = ew.EnterBlockAndGetDelayedLength((_, _) => {
-        this.Data.Write(ew);
-      });
-      actualLengthTask.ContinueWith(
-          length =>
-              beforeLengthTask.SetResult(length.Result));
-    }
+    [Ignore]
+    public T Data => this.impl_.Data;
   }
 }
---
+++ FinModelUtility/Fin/src/schema/data/ISizedSection.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using fin.util.asserts;
+
+using schema;
+using schema.attributes.ignore;
+
+
+namespace fin.schema.data {
+  public interface ISizedSection<T> : IBiSerializable
+      where T : IBiSerializable {
+    T Data { get; }
+  }
+
+  /// <summary>
+  ///   Schema class that implements a uint32-sized section without needing to
+  ///   worry about passing in an instance of the contained data. This should
+  ///   be adequate for most cases, except when the data class needs to access
+  ///   parent data.
+  /// </summary>
+  [BinarySchema]
+  public partial class AutoUInt32SizedSection<T> : ISizedSection<T>
+      where T : IBiSerializable, new() {
+    private readonly PassThruMagicUint32SizedSection<T> impl_;
+
+    [Ignore]
+    public T Data => this.impl_.Data;
+
+    public AutoUInt32SizedSection(string magic) {
+      this.impl_ = new(magic, new T());
+    }
+  }
+
+  public class PassThruUint32SizedSection<T> : ISizedSection<T>
+      where T : IBiSerializable {
+    public T Data { get; }
+
+    public PassThruUint32SizedSection(T data) {
+      this.Data = data;
+    }
+
+    public void Read(EndianBinaryReader er) {
+      var expectedDataSize = er.ReadUInt32();
+
+      var dataStart = er.Position;
+      this.Data.Read(er);
+      var actualDataSize = er.Position - dataStart;
+      if (expectedDataSize != actualDataSize) {
+        Asserts.Fail(
+            $"Expected to read {expectedDataSize} bytes in section, but actually read {actualDataSize} bytes.");
+      }
+    }
+
+    public void Write(EndianBinaryWriter ew) {
+      var beforeLengthTask = new TaskCompletionSource<long>();
+      ew.WriteUInt32Delayed(
+          beforeLengthTask.Task.ContinueWith(
+              length => (uint) length.Result));
+
+      var actualLengthTask = ew.EnterBlockAndGetDelayedLength((_, _) => {
+        this.Data.Write(ew);
+      });
+      actualLengthTask.ContinueWith(
+          length =>
+              beforeLengthTask.SetResult(length.Result));
+    }
+  }
+}
