214f1c24f5410ea09c3855b9f89c1966f92891ba
MeltyPlayer <mastergamepro@hotmail.com>
2022-11-30 23:51:33 -0600

    Added some useful math methods.

--- FinModelUtility/Fin/src/math/FinMath.cs
+++ FinModelUtility/Fin/src/math/FinMath.cs
@@ -3,6 +3,21 @@
 
 namespace fin.math {
   public static class FinMath {
+    public static bool IsInRange<TNumber>(TNumber value,
+                                          TNumber min,
+                                          TNumber max)
+        where TNumber : INumber<TNumber>
+      => min <= value && value <= max;
+
+    public static bool IsInArea<TNumber>(TNumber valueX,
+                                         TNumber valueY,
+                                         TNumber minX,
+                                         TNumber minY,
+                                         TNumber maxX,
+                                         TNumber maxY)
+        where TNumber : INumber<TNumber>
+      => IsInRange(valueX, minX, maxX) && IsInRange(valueY, minY, maxY);
+
     public static TNumber MapRange<TNumber>(TNumber value,
                                             TNumber minFrom,
                                             TNumber maxFrom,
