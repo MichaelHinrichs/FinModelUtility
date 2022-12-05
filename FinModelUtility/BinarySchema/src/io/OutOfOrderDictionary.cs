bfc1eea6cea2fefb65a09c585b9860e851f9e8a5
MeltyPlayer <mastergamepro@hotmail.com>
2022-12-04 23:22:41 -0600

    Created a type that represents a dictionary that can work out-of-order.

---
+++ FinModelUtility/BinarySchema/src/io/OutOfOrderDictionary.cs
@@ -0,0 +1,51 @@
+﻿using schema.util;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace schema.io {
+  public interface IOutOfOrderDictionary<in TKey, TValue>
+      where TKey : notnull {
+    void AssertAllCompleted();
+
+    Task<TValue> Get(TKey key);
+    void Set(TKey key, TValue value);
+  }
+
+  public class OutOfOrderDictionary<TKey, TValue>
+      : IOutOfOrderDictionary<TKey, TValue> {
+    private readonly Dictionary<TKey, TaskCompletionSource<TValue>> impl_ =
+        new();
+
+    public void AssertAllCompleted() {
+      var incompleted = new List<TKey>();
+      foreach (var pair in this.impl_) {
+        if (!pair.Value.Task.IsCompleted) {
+          incompleted.Add(pair.Key);
+        }
+      }
+
+      if (incompleted.Count > 0) {
+        Asserts.Fail(
+            $"Expected for all keys in the out-of-order dictionary to be provided values, but still has {incompleted.Count}/{this.impl_.Count
+            } waiting!");
+      }
+    }
+
+    public Task<TValue> Get(TKey key)
+      => this.GetOrCreateTaskCompletionSource_(key).Task;
+
+    public void Set(TKey key, TValue value)
+      => this.GetOrCreateTaskCompletionSource_(key).SetResult(value);
+
+    private TaskCompletionSource<TValue> GetOrCreateTaskCompletionSource_(
+        TKey key) {
+      if (!this.impl_.TryGetValue(key, out var taskCompletionSource)) {
+        taskCompletionSource = new TaskCompletionSource<TValue>();
+        this.impl_[key] = taskCompletionSource;
+      }
+
+      return taskCompletionSource;
+    }
+  }
+}
