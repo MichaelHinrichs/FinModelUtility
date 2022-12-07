a91424a7dd27f5bfb91f3c9beb8ed7c5d9b6c6c8
MeltyPlayer <mastergamepro@hotmail.com>
2022-12-07 14:54:00 -0600

    Defined Schema attribute for pointers.

---
+++ FinModelUtility/BinarySchema/src/attributes/memory/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace schema.attributes.memory {
+  /// <summary>
+  ///   Pointer that encodes the relative difference between some address and
+  ///   the start of the containing stream.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class PointerToAttribute : BMemberAttribute {
+    private readonly string otherMemberName_;
+
+    public PointerToAttribute(string otherMemberName) {
+      this.otherMemberName_ = otherMemberName;
+    }
+
+    protected override void InitFields() {
+      this.TypeChainToOtherMember =
+          this.GetTypeChainRelativeToStructure(
+              this.otherMemberName_, false);
+    }
+
+    public ITypeChain TypeChainToOtherMember { get; private set; }
+  }
+}
