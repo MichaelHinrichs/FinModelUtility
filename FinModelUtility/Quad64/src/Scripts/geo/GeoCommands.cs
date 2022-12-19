7f8bfb18829711c5099cbc8b6cd1904a6a2aa2bb
MeltyPlayer <mastergamepro@hotmail.com>
2022-12-19 00:36:45 -0600

    Finished setting up the geo layout parsing, hopefully.

--- FinModelUtility/Quad64/src/Scripts/geo/GeoCommands.cs
+++ FinModelUtility/Quad64/src/Scripts/geo/GeoCommands.cs
@@ -11,7 +11,14 @@ namespace sm64.scripts {
     RETURN_FROM_BRANCH = 0x03,
     OPEN_NODE = 0x04,
     CLOSE_NODE = 0x05,
+    VIEWPORT = 0x08,
+    ORTHO_MATRIX = 0x09,
+    CAMERA_FRUSTUM = 0x0A,
+    START_LAYOUT = 0x0B,
+    TOGGLE_DEPTH_BUFFER = 0x0C,
     SET_RENDER_RANGE = 0x0D,
+    SWITCH = 0x0E,
+    CAMERA_LOOK_AT = 0x0F,
     TRANSLATE_AND_ROTATE = 0x10,
     TRANSLATE = 0x11,
     ROTATE = 0x12,
@@ -20,9 +27,11 @@ namespace sm64.scripts {
     DISPLAY_LIST = 0x15,
     SHADOW = 0x16,
     OBJECT_LIST = 0x17,
+    DISPLAY_LIST_FROM_ASM = 0x18,
     BACKGROUND = 0x19,
     NOOP_1A = 0x1A,
     SCALE = 0x1D,
+    CULLING_RADIUS = 0x20,
   }
 
   public enum GeoDrawingLayer : byte {
@@ -34,6 +43,13 @@ namespace sm64.scripts {
     BLENDING1,
     BLENDING2,
     BLENDING3
+  }
+
+  public enum GeoTranslateAndRotateFormat : byte {
+    TRANSLATION_AND_ROTATION,
+    TRANSLATION,
+    ROTATION,
+    YAW
   }
 
   public interface IGeoCommandList {
@@ -106,6 +122,61 @@ namespace sm64.scripts {
   }
 
   [BinarySchema]
+  public partial class GeoViewportCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.VIEWPORT;
+    private readonly byte padding_ = 0;
+
+    public short NumEntries { get; set; }
+    public short X { get; set; }
+    public short Y { get; set; }
+    public short Width { get; set; }
+    public short Height { get; set; }
+  }
+
+  [BinarySchema]
+  public partial class GeoOrthoMatrixCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.ORTHO_MATRIX;
+    private readonly byte padding_ = 0;
+
+    public short NearPlane { get; set; }
+  }
+
+  [BinarySchema]
+  public partial class GeoCameraFrustumCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.CAMERA_FRUSTUM;
+
+    [IntegerFormat(SchemaIntegerType.BYTE)]
+    public bool EnableFrustumFunc { get; private set; }
+
+    public short FieldOfView { get; set; }
+    public short Near { get; set; }
+    public short Far { get; set; }
+
+    [IfBoolean(nameof(EnableFrustumFunc))]
+    public uint? FrustumFuncSegmentedAddress { get; set; }
+  }
+
+  [BinarySchema]
+  public partial class GeoStartLayoutCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.START_LAYOUT;
+
+    [IntegerFormat(SchemaIntegerType.UINT24)]
+    private readonly uint padding_ = 0;
+  }
+
+  [BinarySchema]
+  public partial class GeoToggleDepthBufferCommand
+      : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.TOGGLE_DEPTH_BUFFER;
+
+    [IntegerFormat(SchemaIntegerType.BYTE)]
+    public bool Enable { get; set; }
+
+    private readonly ushort padding_ = 0;
+  }
+
+
+  [BinarySchema]
   public partial class GeoSetRenderRangeCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.SET_RENDER_RANGE;
 
@@ -114,6 +185,27 @@ namespace sm64.scripts {
 
     public short MinimumDistance { get; set; }
     public short MaximumDistance { get; set; }
+  }
+
+  [BinarySchema]
+  public partial class GeoSwitchCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.SWITCH;
+    private readonly byte padding_ = 0;
+
+    public short InitialSelectedCase { get; set; }
+
+    public uint CaseSelectorSegmentedAddress { get; set; }
+  }
+
+  [BinarySchema]
+  public partial class GeoCameraLookAtCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.CAMERA_LOOK_AT;
+    private readonly byte padding_ = 0;
+
+    public short CameraType { get; set; }
+    public Vector3s EyePosition { get; } = new();
+    public Vector3s FocusPosition { get; } = new();
+    public uint Function { get; set; }
   }
 
   public class GeoTranslateAndRotateCommand : IGeoCommand {
@@ -129,13 +221,20 @@ namespace sm64.scripts {
     public GeoDrawingLayer DrawingLayer
       => GeoUtils.GetDrawingLayerFromParams(Params);
 
+    public bool HasDisplayList
+      => GeoUtils.IsDisplayListAndDrawingLayerEnabled(Params);
+
+    public GeoTranslateAndRotateFormat Format
+      => GeoUtils.GetTranslateAndRotateFormat(Params);
+
     public Vector3s Translation => new();
     public Vector3s Rotation => new();
 
     public uint? DisplayListSegmentedAddress { get; set; }
   }
 
-  public class GeoTranslationCommand : IGeoCommand {
+  [BinarySchema]
+  public partial class GeoTranslationCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.TRANSLATE;
 
     /// <summary>
@@ -143,15 +242,22 @@ namespace sm64.scripts {
     /// </summary>
     public byte Params { get; set; }
 
+    [Ignore]
     public GeoDrawingLayer DrawingLayer
       => GeoUtils.GetDrawingLayerFromParams(Params);
 
     public Vector3s Translation => new();
 
+    [Ignore]
+    public bool HasDisplayList
+      => GeoUtils.IsDisplayListAndDrawingLayerEnabled(Params);
+
+    [IfBoolean(nameof(HasDisplayList))]
     public uint? DisplayListSegmentedAddress { get; set; }
   }
 
-  public class GeoRotationCommand : IGeoCommand {
+  [BinarySchema]
+  public partial class GeoRotationCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.ROTATE;
 
     /// <summary>
@@ -159,27 +265,34 @@ namespace sm64.scripts {
     /// </summary>
     public byte Params { get; set; }
 
+    [Ignore]
     public GeoDrawingLayer DrawingLayer
       => GeoUtils.GetDrawingLayerFromParams(Params);
 
     public Vector3s Rotation => new();
 
+    [Ignore]
+    public bool HasDisplayList
+      => GeoUtils.IsDisplayListAndDrawingLayerEnabled(Params);
+
+    [IfBoolean(nameof(HasDisplayList))]
     public uint? DisplayListSegmentedAddress { get; set; }
   }
 
   [BinarySchema]
   public partial class GeoDisplayListWithOffsetCommand
-      : IGeoCommand, IBiSerializable {
+      : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.DISPLAY_LIST_WITH_OFFSET;
 
     public GeoDrawingLayer DrawingLayer { get; set; }
 
     public Vector3s Offset => new();
 
-    public uint? DisplayListSegmentedAddress { get; set; }
+    public uint DisplayListSegmentedAddress { get; set; }
   }
 
-  public class GeoBillboardCommand : IGeoCommand {
+  [BinarySchema]
+  public partial class GeoBillboardCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.BILLBOARD;
 
     /// <summary>
@@ -187,24 +300,31 @@ namespace sm64.scripts {
     /// </summary>
     public byte Params { get; set; }
 
+    [Ignore]
     public GeoDrawingLayer DrawingLayer
       => GeoUtils.GetDrawingLayerFromParams(Params);
 
     public Vector3s Translation => new();
 
+    [Ignore]
+    public bool HasDisplayList
+      => GeoUtils.IsDisplayListAndDrawingLayerEnabled(Params);
+
+    [IfBoolean(nameof(HasDisplayList))]
     public uint? DisplayListSegmentedAddress { get; set; }
   }
 
   [BinarySchema]
-  public partial class GeoDisplayListCommand : IGeoCommand, IBiSerializable {
+  public partial class GeoDisplayListCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.DISPLAY_LIST;
-
     public GeoDrawingLayer DrawingLayer { get; set; }
+    private readonly ushort padding_ = 0;
+
     public uint DisplayListSegmentedAddress { get; set; }
   }
 
   [BinarySchema]
-  public partial class GeoShadowCommand : IGeoCommand, IBiSerializable {
+  public partial class GeoShadowCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.SHADOW;
     private readonly byte padding_ = 0;
 
@@ -214,7 +334,7 @@ namespace sm64.scripts {
   }
 
   [BinarySchema]
-  public partial class GeoObjectListCommand : IGeoCommand, IBiSerializable {
+  public partial class GeoObjectListCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.OBJECT_LIST;
 
     [IntegerFormat(SchemaIntegerType.UINT24)]
@@ -222,7 +342,17 @@ namespace sm64.scripts {
   }
 
   [BinarySchema]
-  public partial class GeoBackgroundCommand : IGeoCommand, IBiSerializable {
+  public partial class GeoDisplayListFromAsm : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.DISPLAY_LIST_FROM_ASM;
+
+    private readonly byte padding_ = 0;
+
+    public ushort Params { get; set; }
+    public uint AsmSegmentedAddress { get; set; }
+  }
+
+  [BinarySchema]
+  public partial class GeoBackgroundCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.BACKGROUND;
 
     private readonly byte padding_ = 0;
@@ -233,7 +363,7 @@ namespace sm64.scripts {
   }
 
   [BinarySchema]
-  public partial class GeoNoopCommand : IGeoCommand, IBiSerializable {
+  public partial class GeoNoopCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id { get; set; }
 
     [IntegerFormat(SchemaIntegerType.UINT24)]
@@ -242,7 +372,8 @@ namespace sm64.scripts {
     private readonly uint padding2_ = 0;
   }
 
-  public class GeoScaleCommand : IGeoCommand {
+  [BinarySchema]
+  public partial class GeoScaleCommand : IGeoCommand, IDeserializable {
     public GeoCommandId Id => GeoCommandId.SCALE;
 
     /// <summary>
@@ -250,16 +381,29 @@ namespace sm64.scripts {
     /// </summary>
     public byte Params { get; set; }
 
+    [Ignore]
     public GeoDrawingLayer DrawingLayer
       => GeoUtils.GetDrawingLayerFromParams(Params);
 
+    private readonly ushort padding_ = 0;
+
     public uint Scale { get; set; }
 
+    [Ignore]
+    public bool HasDisplayList
+      => GeoUtils.IsDisplayListAndDrawingLayerEnabled(Params);
+
+    [IfBoolean(nameof(HasDisplayList))]
     public uint? DisplayListSegmentedAddress { get; set; }
   }
 
-  public class GeoNotImplementedCommand : IGeoCommand {
-    public GeoCommandId Id { get; set; }
+  [BinarySchema]
+  public partial class GeoCullingRadiusCommand : IGeoCommand, IDeserializable {
+    public GeoCommandId Id => GeoCommandId.CULLING_RADIUS;
+
+    private readonly byte padding_ = 0;
+
+    public short CullingRadius { get; set; }
   }
 
   public static class GeoUtils {
@@ -269,5 +413,9 @@ namespace sm64.scripts {
     public static GeoDrawingLayer GetDrawingLayerFromParams(byte param)
       => (GeoDrawingLayer)
           (IsDisplayListAndDrawingLayerEnabled(param) ? param & 0xF : 0);
+
+    public static GeoTranslateAndRotateFormat GetTranslateAndRotateFormat(
+        byte param)
+      => (GeoTranslateAndRotateFormat)((param & 0x70) >> 4);
   }
 }
