805a445e13b7c4ec362bbdc44d3ca3a909a351a4
MeltyPlayer <mastergamepro@hotmail.com>
2022-12-29 02:13:20 -0600

    Create FinStack.cs

---
+++ FinModelUtility/Fin/src/data/stack/FinStack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace fin.data.stack {
+  public interface IStack<T> {
+    int Count { get; }
+    T Top { get; set; }
+
+    void Clear();
+
+    bool TryPop(out T item);
+    T Pop();
+    void Push(T item);
+  }
+
+  public class FinStack<T> : IStack<T> {
+    private readonly Stack<T> impl_;
+
+    public FinStack() {
+      this.impl_ = new Stack<T>();
+    }
+
+    public FinStack(T item) {
+      this.impl_ = new Stack<T>();
+      this.impl_.Push(item);
+    }
+
+    public FinStack(IEnumerable<T> items) {
+      this.impl_ = new Stack<T>(items);
+    }
+
+    public int Count => this.impl_.Count;
+
+    public T Top {
+      get => this.impl_.Peek();
+      set {
+        this.impl_.Pop();
+        this.impl_.Push(value);
+      }
+    }
+
+    public bool TryPop(out T item) => this.impl_.TryPop(out item);
+    public T Pop() => this.impl_.Pop();
+    public void Push(T item) => this.impl_.Push(item);
+  }
+}
