22f74222f2f58bbf469cf07df5ad238e58385f62
MeltyPlayer <mastergamepro@hotmail.com>
2023-01-15 11:45:17 -0600

    Switched uses of Math to MathF in the QuaternionUtil file.

--- FinModelUtility/Fin/src/math/QuaternionUtil.cs
+++ FinModelUtility/Fin/src/math/QuaternionUtil.cs
@@ -26,22 +26,22 @@ namespace fin.math {
       Vector3 angles = new();
 
       // roll / x
-      double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-      double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-      angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
+      var sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
+      var cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
+      angles.X = MathF.Atan2(sinr_cosp, cosr_cosp);
 
       // pitch / y
-      double sinp = 2 * (q.W * q.Y - q.Z * q.X);
+      var sinp = (float) (2 * (q.W * q.Y - q.Z * q.X));
       if (Math.Abs(sinp) >= 1) {
-        angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
+        angles.Y = MathF.CopySign(MathF.PI / 2, sinp);
       } else {
-        angles.Y = (float)Math.Asin(sinp);
+        angles.Y = MathF.Asin(sinp);
       }
 
       // yaw / z
-      double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-      double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-      angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
+      var siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
+      var cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+      angles.Z = MathF.Atan2(siny_cosp, cosy_cosp);
 
       return angles;
     }
