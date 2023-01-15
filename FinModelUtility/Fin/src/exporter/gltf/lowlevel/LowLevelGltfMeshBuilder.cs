0ce386da396d4869e59c2c448522fec6441d373b
MeltyPlayer <mastergamepro@hotmail.com>
2023-01-15 12:46:11 -0600

    Fixed a really embarrassing bug in the low-level GLTF exporter that was causing models to take up something like 10x more memory than they should.

--- FinModelUtility/Fin/src/exporter/gltf/lowlevel/LowLevelGltfMeshBuilder.cs
+++ FinModelUtility/Fin/src/exporter/gltf/lowlevel/LowLevelGltfMeshBuilder.cs
@@ -41,6 +41,134 @@ namespace fin.exporter.gltf.lowlevel {
 
       var DEFAULT_SKINNING = SparseWeight8.Create((0, 1));
 
+      var points = model.Skin.Vertices;
+      var pointsCount = points.Count;
+
+      var positionView = gltfModel.CreateBufferView(
+          4 * 3 * pointsCount, 0,
+          BufferMode.ARRAY_BUFFER);
+      var positionArray = new Vector3Array(positionView.Content);
+
+      var normalView = gltfModel.CreateBufferView(
+          4 * 3 * pointsCount, 0,
+          BufferMode.ARRAY_BUFFER);
+      var normalArray = new Vector3Array(normalView.Content);
+
+      var colorView = gltfModel.CreateBufferView(
+          4 * 4 * pointsCount, 0,
+          BufferMode.ARRAY_BUFFER);
+      var colorArray = new ColorArray(colorView.Content);
+
+      var uvView = gltfModel.CreateBufferView(
+          4 * 2 * pointsCount, 0,
+          BufferMode.ARRAY_BUFFER);
+      var uvArray = new Vector2Array(uvView.Content);
+
+      for (var p = 0; p < pointsCount; ++p) {
+        var point = points[p];
+
+        boneTransformManager.ProjectVertex(point, outPosition, outNormal);
+        var pos = positionArray[p];
+        pos.X = outPosition.X;
+        pos.Y = outPosition.Y;
+        pos.Z = outPosition.Z;
+        positionArray[p] = pos;
+
+        if (point.LocalNormal != null) {
+          var norm = normalArray[p];
+
+          norm.X = outNormal.X;
+          norm.Y = outNormal.Y;
+          norm.Z = outNormal.Z;
+          normalArray[p] = norm;
+        }
+
+        var finColor0 = point.GetColor(0);
+        var hasColor0 = finColor0 != null;
+        var assColor0 = hasColor0
+                            ? LowLevelGltfMeshBuilder.FinToGltfColor_(
+                                finColor0)
+                            : new Vector4(1, 1, 1, 1);
+        var col = colorArray[p];
+        col.X = assColor0.X;
+        col.Y = assColor0.Y;
+        col.Z = assColor0.Z;
+        col.W = assColor0.W;
+        colorArray[p] = col;
+
+        var finUv = point.GetUv(0);
+        var uv = uvArray[p];
+        uv.X = finUv.U;
+        uv.Y = finUv.V;
+        uvArray[p] = uv;
+
+        /*if (point.Weights != null) {
+          vertexBuilder = vertexBuilder.WithSkinning(
+              point.Weights.Select(
+                       boneWeight
+                           => (boneToIndex[boneWeight.Bone],
+                               boneWeight.Weight))
+                   .ToArray());
+        } else {
+          vertexBuilder = vertexBuilder.WithSkinning(DEFAULT_SKINNING);
+        }
+
+        if (point.LocalNormal != null) {
+          var tangent = point.LocalTangent;
+
+          if (tangent == null) {
+            vertexBuilder = vertexBuilder.WithGeometry(
+                position,
+                new Vector3(outNormal.X, outNormal.Y, outNormal.Z));
+          } else {
+            vertexBuilder = vertexBuilder.WithGeometry(
+                position,
+                new Vector3(outNormal.X, outNormal.Y, outNormal.Z),
+                new Vector4(tangent.X, tangent.Y, tangent.Z, tangent.W));
+          }
+        }
+
+        var finColor0 = point.GetColor(0);
+        var hasColor0 = finColor0 != null;
+        var assColor0 = hasColor0
+                            ? LowLevelGltfMeshBuilder.FinToGltfColor_(
+                                finColor0)
+                            : new Vector4(1, 1, 1, 1);
+        var finColor1 = point.GetColor(1);
+        var hasColor1 = finColor1 != null;
+        var assColor1 = hasColor1
+                            ? LowLevelGltfMeshBuilder.FinToGltfColor_(
+                                finColor1)
+                            : new Vector4(1, 1, 1, 1);
+
+        var hasColor = hasColor0 || hasColor1;
+
+        var uvs = point.Uvs;
+        var hasUvs = (uvs?.Count ?? 0) > 0;
+        if (!this.UvIndices) {
+          if (hasUvs) {
+            var uv = uvs[0];
+            vertexBuilder =
+                vertexBuilder.WithMaterial(assColor0,
+                                           assColor1,
+                                           new Vector2(uv.U, uv.V));
+          } else if (hasColor) {
+            vertexBuilder =
+                vertexBuilder.WithMaterial(assColor0, assColor1);
+          }
+        } else {
+          // Importing the color directly via Assimp doesn't work for some
+          // reason.
+          vertexBuilder =
+              vertexBuilder.WithMaterial(new Vector4(1, 1, 1, 1),
+                                         new Vector2(
+                                             hasUvs ? point.Index : -1,
+                                             hasColor ? point.Index : -1));
+        }
+
+        vertices[p] = vertexBuilder;*/
+      }
+
       var gltfMeshes = new List<Mesh>();
       foreach (var finMesh in skin.Meshes) {
         var gltfMesh = gltfModel.CreateMesh(finMesh.Name);
@@ -52,134 +180,6 @@ namespace fin.exporter.gltf.lowlevel {
                 finToTexCoordAndGltfMaterial[primitive.Material];
           } else {
             material = nullMaterial;
-          }
-
-          var points = primitive.Vertices;
-          var pointsCount = points.Count;
-
-          var positionView = gltfModel.CreateBufferView(
-              4 * 3 * pointsCount, 0,
-              BufferMode.ARRAY_BUFFER);
-          var positionArray = new Vector3Array(positionView.Content);
-
-          var normalView = gltfModel.CreateBufferView(
-              4 * 3 * pointsCount, 0,
-              BufferMode.ARRAY_BUFFER);
-          var normalArray = new Vector3Array(normalView.Content);
-
-          var colorView = gltfModel.CreateBufferView(
-              4 * 4 * pointsCount, 0,
-              BufferMode.ARRAY_BUFFER);
-          var colorArray = new ColorArray(colorView.Content);
-
-          var uvView = gltfModel.CreateBufferView(
-              4 * 2 * pointsCount, 0,
-              BufferMode.ARRAY_BUFFER);
-          var uvArray = new Vector2Array(uvView.Content);
-
-          for (var p = 0; p < pointsCount; ++p) {
-            var point = points[p];
-
-            boneTransformManager.ProjectVertex(point, outPosition, outNormal);
-            var pos = positionArray[p];
-            pos.X = outPosition.X;
-            pos.Y = outPosition.Y;
-            pos.Z = outPosition.Z;
-            positionArray[p] = pos;
-
-            if (point.LocalNormal != null) {
-              var norm = normalArray[p];
-
-              norm.X = outNormal.X;
-              norm.Y = outNormal.Y;
-              norm.Z = outNormal.Z;
-              normalArray[p] = norm;
-            }
-
-            var finColor0 = point.GetColor(0);
-            var hasColor0 = finColor0 != null;
-            var assColor0 = hasColor0
-                                ? LowLevelGltfMeshBuilder.FinToGltfColor_(
-                                    finColor0)
-                                : new Vector4(1, 1, 1, 1);
-            var col = colorArray[p];
-            col.X = assColor0.X;
-            col.Y = assColor0.Y;
-            col.Z = assColor0.Z;
-            col.W = assColor0.W;
-            colorArray[p] = col;
-
-            var finUv = point.GetUv(0);
-            var uv = uvArray[p];
-            uv.X = finUv.U;
-            uv.Y = finUv.V;
-            uvArray[p] = uv;
-
-            /*if (point.Weights != null) {
-              vertexBuilder = vertexBuilder.WithSkinning(
-                  point.Weights.Select(
-                           boneWeight
-                               => (boneToIndex[boneWeight.Bone],
-                                   boneWeight.Weight))
-                       .ToArray());
-            } else {
-              vertexBuilder = vertexBuilder.WithSkinning(DEFAULT_SKINNING);
-            }
-
-            if (point.LocalNormal != null) {
-              var tangent = point.LocalTangent;
-
-              if (tangent == null) {
-                vertexBuilder = vertexBuilder.WithGeometry(
-                    position,
-                    new Vector3(outNormal.X, outNormal.Y, outNormal.Z));
-              } else {
-                vertexBuilder = vertexBuilder.WithGeometry(
-                    position,
-                    new Vector3(outNormal.X, outNormal.Y, outNormal.Z),
-                    new Vector4(tangent.X, tangent.Y, tangent.Z, tangent.W));
-              }
-            }
-
-            var finColor0 = point.GetColor(0);
-            var hasColor0 = finColor0 != null;
-            var assColor0 = hasColor0
-                                ? LowLevelGltfMeshBuilder.FinToGltfColor_(
-                                    finColor0)
-                                : new Vector4(1, 1, 1, 1);
-            var finColor1 = point.GetColor(1);
-            var hasColor1 = finColor1 != null;
-            var assColor1 = hasColor1
-                                ? LowLevelGltfMeshBuilder.FinToGltfColor_(
-                                    finColor1)
-                                : new Vector4(1, 1, 1, 1);
-
-            var hasColor = hasColor0 || hasColor1;
-
-            var uvs = point.Uvs;
-            var hasUvs = (uvs?.Count ?? 0) > 0;
-            if (!this.UvIndices) {
-              if (hasUvs) {
-                var uv = uvs[0];
-                vertexBuilder =
-                    vertexBuilder.WithMaterial(assColor0,
-                                               assColor1,
-                                               new Vector2(uv.U, uv.V));
-              } else if (hasColor) {
-                vertexBuilder =
-                    vertexBuilder.WithMaterial(assColor0, assColor1);
-              }
-            } else {
-              // Importing the color directly via Assimp doesn't work for some
-              // reason.
-              vertexBuilder =
-                  vertexBuilder.WithMaterial(new Vector4(1, 1, 1, 1),
-                                             new Vector2(
-                                                 hasUvs ? point.Index : -1,
-                                                 hasColor ? point.Index : -1));
-            }
-
-            vertices[p] = vertexBuilder;*/
           }
 
           var gltfPrimitive = gltfMesh.CreatePrimitive();
@@ -206,19 +206,21 @@ namespace fin.exporter.gltf.lowlevel {
               uvView, 0, pointsCount, DimensionType.VEC2);
           gltfPrimitive.SetVertexAccessor("TEXCOORD_0", uvAccessor);
 
+          var primitivePoints = primitive.Vertices;
+
           switch (primitive.Type) {
             case PrimitiveType.TRIANGLES: {
               gltfPrimitive.DrawPrimitiveType =
                   SharpGLTF.Schema2.PrimitiveType.TRIANGLES;
 
-              var indexView = gltfModel.CreateBufferView(4 * pointsCount, 0,
+              var indexView = gltfModel.CreateBufferView(4 * primitivePoints.Count, 0,
                 BufferMode.ELEMENT_ARRAY_BUFFER);
               var indexArray = new IntegerArray(indexView.Content);
 
-              for (var v = 0; v < pointsCount; v += 3) {
-                var v1 = v + 0;
-                var v2 = v + 1;
-                var v3 = v + 2;
+              for (var v = 0; v < primitivePoints.Count; v += 3) {
+                var v1 = primitivePoints[v + 0].Index;
+                var v2 = primitivePoints[v + 1].Index;
+                var v3 = primitivePoints[v + 2].Index;
 
                 if (primitive.VertexOrder == VertexOrder.FLIP) {
                   indexArray[v + 0] = (uint) v1;
@@ -232,7 +234,7 @@ namespace fin.exporter.gltf.lowlevel {
               }
 
               var indexAccessor = gltfModel.CreateAccessor();
-              indexAccessor.SetIndexData(indexView, 0, pointsCount,
+              indexAccessor.SetIndexData(indexView, 0, primitivePoints.Count,
                                          IndexEncodingType.UNSIGNED_INT);
 
               gltfPrimitive.SetIndexAccessor(indexAccessor);
