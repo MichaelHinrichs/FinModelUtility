e9ddabe9c75610a709a90acdaeb6c19a4824423d
MeltyPlayer <mastergamepro@hotmail.com>
2023-01-25 22:44:48 -0600

    Cached the value of IsOppositeEndiannessOfSystem to speed things up a bit when reading--this is a cause of slowdown, apparently.

--- FinModelUtility/BinarySchema/lib/system/io/Endianness.cs
+++ FinModelUtility/BinarySchema/lib/system/io/Endianness.cs
@@ -22,8 +22,8 @@ namespace System.IO {
   public static class EndiannessUtil {
     public static Endianness SystemEndianness
       => BitConverter.IsLittleEndian
-             ? Endianness.LittleEndian
-             : Endianness.BigEndian;
+          ? Endianness.LittleEndian
+          : Endianness.BigEndian;
   }
 
   public interface IEndiannessStack {
@@ -52,29 +52,40 @@ namespace System.IO {
     public Endianness Endianness
       => this.endiannessStack_.Peek()?.Item2 ?? EndiannessUtil.SystemEndianness;
 
-    public bool IsOppositeEndiannessOfSystem =>
-        this.Endianness != EndiannessUtil.SystemEndianness;
+    public bool IsOppositeEndiannessOfSystem { get; private set; }
 
     /// <summary>
     ///   Pushes a structure's endianness. This will override the reader/writer
     ///   endianness, but will be overwritten by the field endianness if those
     ///   were already pushed.
     /// </summary>
-    public void PushStructureEndianness(Endianness endianness)
-      => this.endiannessStack_.Push(
+    public void PushStructureEndianness(Endianness endianness) {
+      this.endiannessStack_.Push(
           PickSuperior_(
               this.endiannessStack_.Peek(),
               (EndiannessSource.STRUCTURE, endianness)));
-
+      this.UpdateReverse_();
+    }
+    
     /// <summary>
     ///   Pushes a field's endianness. This will override any other
     ///   endiannesses that were previously pushed.
     /// </summary>
     /// <param name="endianness"></param>
-    public void PushMemberEndianness(Endianness endianness)
-      => this.endiannessStack_.Push((EndiannessSource.MEMBER, endianness));
+    public void PushMemberEndianness(Endianness endianness) {
+      this.endiannessStack_.Push((EndiannessSource.MEMBER, endianness));
+      this.UpdateReverse_();
+    }
 
-    public void PopEndianness() => this.endiannessStack_.Pop();
+    public void PopEndianness() {
+      this.endiannessStack_.Pop();
+      this.UpdateReverse_();
+    }
+
+    private void UpdateReverse_() {
+      IsOppositeEndiannessOfSystem =
+          this.Endianness != EndiannessUtil.SystemEndianness;
+    }
 
     private static (EndiannessSource, Endianness)? PickSuperior_(
         (EndiannessSource, Endianness)? prev,
