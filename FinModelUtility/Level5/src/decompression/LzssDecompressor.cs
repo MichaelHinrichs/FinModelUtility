7cb2be4f2d2e1404269e5eb68160373dcceccb86
MeltyPlayer <mastergamepro@hotmail.com>
2023-02-03 16:21:25 -0600

    Cleaned up the LZSS Decompressor code a bit.

--- FinModelUtility/Level5/src/decompression/LzssDecompressor.cs
+++ FinModelUtility/Level5/src/decompression/LzssDecompressor.cs
@@ -3,29 +3,31 @@
 
 namespace level5.decompression {
   public class LzssDecompressor : BDecompressor {
-    public override bool TryDecompress(byte[] src, out byte[] dst) {
+    public override unsafe bool TryDecompress(byte[] src, out byte[] dst) {
       var size = 0;
       {
-        int p = 4;
+        int srcIndex = 4;
         int op = 0;
 
         int mask = 0;
         int flag = 0;
 
-        while (p < src.Length) {
+        while (srcIndex < src.Length) {
           if (mask == 0) {
-            flag = (src[p++] & 0xFF);
+            flag = src[srcIndex++];
             mask = 0x80;
           }
 
           if ((flag & mask) == 0) {
-            if (p + 1 > src.Length) break;
-            p++;
+            if (srcIndex + 1 > src.Length) break;
+            srcIndex++;
             size++;
             op++;
           } else {
-            if (p + 2 > src.Length) break;
-            int dat = ((src[p++] & 0xFF) << 8) | (src[p++] & 0xFF);
+            if (srcIndex + 2 > src.Length) break;
+
+
+            var dat = (src[srcIndex++] << 8) | src[srcIndex++];
             int pos = (dat & 0x0FFF) + 1;
 
             if (op >= pos) {
@@ -33,56 +35,62 @@ namespace level5.decompression {
               size += length;
               op += length;
             }
-
-            /*for (int i = 0; i < length; i++) {
-              if (op - pos >= 0) {
-                size++;
-                op++;
-              }
-            }*/
           }
+
           mask >>= 1;
         }
       }
 
       dst = new byte[size];
-      var oI = 0;
       {
-        int p = 4;
+        var dstIndex = 0;
+
+        int srcIndex = 4;
         int op = 0;
 
         int mask = 0;
         int flag = 0;
 
-        while (p < src.Length) {
+        while (srcIndex < src.Length) {
           if (mask == 0) {
-            flag = (src[p++] & 0xFF);
+            flag = src[srcIndex++];
             mask = 0x80;
           }
 
           if ((flag & mask) == 0) {
-            if (p + 1 > src.Length) break;
-            dst[oI++] = src[p++];
+            if (srcIndex + 1 > src.Length) break;
+            dst[dstIndex++] = src[srcIndex++];
             op++;
           } else {
-            if (p + 2 > src.Length) break;
-            int dat = ((src[p++] & 0xFF) << 8) | (src[p++] & 0xFF);
+            if (srcIndex + 2 > src.Length) break;
+            var dat = (src[srcIndex++] << 8) | src[srcIndex++];
             int pos = (dat & 0x0FFF) + 1;
 
-            if (op >= pos) {
-              int length = (dat >> 12) + 3;
-              for (int i = 0; i < length; i++) {
-                dst[oI] = dst[op - pos >= oI ? 0 : op - pos];
-                oI++;
-                op++;
-              }
-            }
+            this.Copy_(dst, ref dstIndex, dat, ref op, pos);
           }
+
           mask >>= 1;
         }
       }
 
       return true;
+    }
+
+    private void Copy_(
+        byte[] dst,
+        ref int dstIndex,
+        int dat,
+        ref int op,
+        int pos
+    ) {
+      if (op >= pos) {
+        int length = (dat >> 12) + 3;
+        for (int i = 0; i < length; i++) {
+          dst[dstIndex] = dst[op - pos >= dstIndex ? 0 : op - pos];
+          dstIndex++;
+          op++;
+        }
+      }
     }
   }
 }
