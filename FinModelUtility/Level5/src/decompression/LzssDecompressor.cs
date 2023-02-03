a6d81fdbe07aa3a083c496ce8d188cda31d125bc
MeltyPlayer <mastergamepro@hotmail.com>
2023-02-03 15:17:56 -0600

    Sped up LzssDecompressor by precalculating the destination size.

--- FinModelUtility/Level5/src/decompression/LzssDecompressor.cs
+++ FinModelUtility/Level5/src/decompression/LzssDecompressor.cs
@@ -4,41 +4,84 @@
 namespace level5.decompression {
   public class LzssDecompressor : BDecompressor {
     public override bool TryDecompress(byte[] src, out byte[] dst) {
-      List<byte> o = new List<byte>();
+      var size = 0;
+      {
+        int p = 4;
+        int op = 0;
 
-      int p = 4;
-      int op = 0;
+        int mask = 0;
+        int flag = 0;
 
-      int mask = 0;
-      int flag = 0;
-
-      while (p < src.Length) {
-        if (mask == 0) {
-          flag = (src[p++] & 0xFF);
-          mask = 0x80;
-        }
-
-        if ((flag & mask) == 0) {
-          if (p + 1 > src.Length) break;
-          o.Add(src[p++]);
-          op++;
-        } else {
-          if (p + 2 > src.Length) break;
-          int dat = ((src[p++] & 0xFF) << 8) | (src[p++] & 0xFF);
-          int pos = (dat & 0x0FFF) + 1;
-          int length = (dat >> 12) + 3;
-
-          for (int i = 0; i < length; i++) {
-            if (op - pos >= 0) {
-              o.Add(o[op - pos >= o.Count ? 0 : op - pos]);
-              op++;
-            }
+        while (p < src.Length) {
+          if (mask == 0) {
+            flag = (src[p++] & 0xFF);
+            mask = 0x80;
           }
+
+          if ((flag & mask) == 0) {
+            if (p + 1 > src.Length) break;
+            p++;
+            size++;
+            op++;
+          } else {
+            if (p + 2 > src.Length) break;
+            int dat = ((src[p++] & 0xFF) << 8) | (src[p++] & 0xFF);
+            int pos = (dat & 0x0FFF) + 1;
+
+            if (op >= pos) {
+              int length = (dat >> 12) + 3;
+              size += length;
+              op += length;
+            }
+
+            /*for (int i = 0; i < length; i++) {
+              if (op - pos >= 0) {
+                size++;
+                op++;
+              }
+            }*/
+          }
+          mask >>= 1;
         }
-        mask >>= 1;
       }
 
-      dst = o.ToArray();
+      dst = new byte[size];
+      var oI = 0;
+      {
+        int p = 4;
+        int op = 0;
+
+        int mask = 0;
+        int flag = 0;
+
+        while (p < src.Length) {
+          if (mask == 0) {
+            flag = (src[p++] & 0xFF);
+            mask = 0x80;
+          }
+
+          if ((flag & mask) == 0) {
+            if (p + 1 > src.Length) break;
+            dst[oI++] = src[p++];
+            op++;
+          } else {
+            if (p + 2 > src.Length) break;
+            int dat = ((src[p++] & 0xFF) << 8) | (src[p++] & 0xFF);
+            int pos = (dat & 0x0FFF) + 1;
+
+            if (op >= pos) {
+              int length = (dat >> 12) + 3;
+              for (int i = 0; i < length; i++) {
+                dst[oI] = dst[op - pos >= oI ? 0 : op - pos];
+                oI++;
+                op++;
+              }
+            }
+          }
+          mask >>= 1;
+        }
+      }
+
       return true;
     }
   }
