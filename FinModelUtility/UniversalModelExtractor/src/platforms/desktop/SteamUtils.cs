0a689acf07923cb435347dcab07d22b0cf53a0a3
MeltyPlayer <mastergamepro@hotmail.com>
2023-02-10 17:18:31 -0600

    Allowed for Steam to not be installed on the current machine when looking for supported games, whoops.

--- FinModelUtility/UniversalModelExtractor/src/platforms/desktop/SteamUtils.cs
+++ FinModelUtility/UniversalModelExtractor/src/platforms/desktop/SteamUtils.cs
@@ -1,7 +1,9 @@
 ﻿using fin.io;
 using fin.util.asserts;
+
 using Gameloop.Vdf;
 using Gameloop.Vdf.Linq;
+
 using Microsoft.Win32;
 
 
@@ -14,7 +16,7 @@ namespace uni.platforms.desktop {
         @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam";
 
 
-    public static string InstallPath { get; } =
+    public static string? InstallPath { get; } =
       (Registry.GetValue(SteamUtils.INSTALL_PATH_64_BIT_REGISTRY_KEY,
                          "InstallPath",
                          null) ??
@@ -22,25 +24,31 @@ namespace uni.platforms.desktop {
                          "InstallPath",
                          null)) as string;
 
-    public static IDirectory InstallDirectory { get; } =
-      new FinDirectory(Asserts.CastNonnull(SteamUtils.InstallPath));
+    public static IDirectory? InstallDirectory { get; } =
+      SteamUtils.InstallPath != null
+          ? new FinDirectory(SteamUtils.InstallPath)
+          : null;
 
     public static IDirectory[] CommonDirectories { get; } =
-      VdfConvert
-          .Deserialize(
-              SteamUtils.InstallDirectory
-                        .GetExistingFile("config/libraryfolders.vdf")
-                        .OpenReadAsText())
-          .Value
-          .Children()
-          .Select(section => section.Value<VProperty>().Value)
-          .Select(section => section["path"])
-          .Select(path => new FinDirectory(path.ToString()))
-          .Where(steamDirectory => steamDirectory.Exists) // A steam directory may not exist if it corresponds to an external hard drive
-          .Select(
-              libraryFolder => libraryFolder.GetSubdir("steamapps"))
-          .Select(steamApps => steamApps.GetSubdir("common"))
-          .ToArray();
+      SteamUtils.InstallDirectory == null
+          ? Array.Empty<IDirectory>()
+          : VdfConvert
+            .Deserialize(
+                SteamUtils.InstallDirectory
+                          .GetExistingFile("config/libraryfolders.vdf")
+                          .OpenReadAsText())
+            .Value
+            .Children()
+            .Select(section => section.Value<VProperty>().Value)
+            .Select(section => section["path"])
+            .Select(path => new FinDirectory(path.ToString()))
+            .Where(steamDirectory
+                       => steamDirectory
+                           .Exists) // A steam directory may not exist if it corresponds to an external hard drive
+            .Select(
+                libraryFolder => libraryFolder.GetSubdir("steamapps"))
+            .Select(steamApps => steamApps.GetSubdir("common"))
+            .ToArray();
 
     public static IDirectory[] GameDirectories { get; }
       = CommonDirectories
@@ -51,9 +59,10 @@ namespace uni.platforms.desktop {
         GetGameDirectory(string name, bool assert = false) {
       var gameDir = GameDirectories.FirstOrDefault(game => game.Name == name);
       return !assert
-                 ? gameDir
-                 : Asserts.CastNonnull(
-                     gameDir, $"Could not find \"{name}\" installed in Steam.");
+          ? gameDir
+          : Asserts.CastNonnull(
+              gameDir,
+              $"Could not find \"{name}\" installed in Steam.");
     }
   }
 }
