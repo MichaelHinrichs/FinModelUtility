d1d3b094b0dc87af4f7d81b8519dd763f35139c4
MeltyPlayer <mastergamepro@hotmail.com>
2023-03-29 06:58:46 -0500

    Set up interfaces for a new image file bundle interface, which will be used to support showing images in the viewer.

---
+++ FinModelUtility/Fin/src/image/IImageLoader.cs
@@ -0,0 +1,6 @@
+﻿namespace fin.image {
+  public interface IImageLoader<TImageFileBundle>
+      where TImageFileBundle : IImageFileBundle {
+    IImage LoadImage(TImageFileBundle imageFileBundle);
+  }
+}
