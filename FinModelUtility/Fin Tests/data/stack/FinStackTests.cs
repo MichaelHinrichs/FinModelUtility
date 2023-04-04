b58c0259a2bc52b5f94438b5027fd77b6887f27e
MeltyPlayer <mastergamepro@hotmail.com>
2023-04-03 21:53:07 -0500

    Added tests for FinStack.

---
+++ FinModelUtility/Fin Tests/data/stack/FinStackTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+using schema.binary.util;
+
+
+namespace fin.data.stack {
+  public class FinStackTests {
+    [Test]
+    public void TestPush() {
+      var stack = new FinStack<string>();
+      Assert.Zero(stack.Count);
+      
+      stack.Push("foo");
+      Assert.AreEqual(1, stack.Count);
+    }
+
+    [Test]
+    public void TestPop() {
+      var stack = new FinStack<string>();
+      stack.Push("foo");
+      stack.Push("bar");
+      Assert.AreEqual(2, stack.Count);
+ 
+      Assert.AreEqual("bar", stack.Pop());
+      Assert.AreEqual(1, stack.Count);
+
+      Assert.AreEqual("foo", stack.Pop());
+      Assert.Zero(stack.Count);
+    }
+
+    [Test]
+    public void TestTryPop() {
+      var stack = new FinStack<string>();
+      stack.Push("foo");
+      stack.Push("bar");
+      Assert.AreEqual(2, stack.Count);
+
+      Assert.True(stack.TryPop(out var value0));
+      Assert.AreEqual("bar", value0);
+      Assert.AreEqual(1, stack.Count);
+
+      Assert.True(stack.TryPop(out var value1));
+      Assert.AreEqual("foo", value1);
+      Assert.Zero(stack.Count);
+
+      Assert.False(stack.TryPop(out _));
+    }
+
+    [Test]
+    public void TestTop() {
+      var stack = new FinStack<string>();
+      
+      stack.Push("foo");
+      Assert.AreEqual("foo", stack.Top);
+
+      stack.Push("bar");
+      Assert.AreEqual("bar", stack.Top);
+
+      stack.Top = "gar";
+      Assert.AreEqual("gar", stack.Pop());
+
+      stack.Top = "goo";
+      Assert.AreEqual("goo", stack.Pop());
+    }
+
+    [Test]
+    public void TestClear() {
+      var stack = new FinStack<string>();
+
+      stack.Push("foo");
+      stack.Push("bar");
+
+      stack.Clear();
+      Assert.Zero(stack.Count);
+      Assert.False(stack.TryPop(out _));
+    }
+  }
+}
