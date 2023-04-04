98c8ebe040db3e567b5f9901ddecc70b75dde107
MeltyPlayer <mastergamepro@hotmail.com>
2023-04-03 22:32:48 -0500

    Added some bit logic and quaternion tests.

--- FinModelUtility/Fin Tests/math/BitLogicTests.cs
+++ FinModelUtility/Fin Tests/math/BitLogicTests.cs
@@ -12,5 +12,21 @@ namespace fin.math {
                       BitLogic.ExtractFromRight(0b11110000, 4, 4));
       Assert.AreEqual((uint) 0b1011, BitLogic.ExtractFromRight(0b101100, 2, 4));
     }
+
+    [Test]
+    [TestCase(1, 0, true)]
+    [TestCase(2, 0, false)]
+    [TestCase(2, 1, true)]
+    public void TestGetBitInt(int value, int bit, bool expectedValue)
+      => Assert.AreEqual(expectedValue, value.GetBit(bit));
+
+    [Test]
+    [TestCase(0, 0)]
+    [TestCase(1, 0b1)]
+    [TestCase(2, 0b11)]
+    [TestCase(3, 0b111)]
+    [TestCase(4, 0b1111)]
+    public void TestMask(int numBits, int expectedMask)
+      => Assert.AreEqual(expectedMask, (int) BitLogic.GetMask(numBits));
   }
 }
--- FinModelUtility/Fin Tests/math/QuaternionUtilTests.cs
+++ FinModelUtility/Fin Tests/math/QuaternionUtilTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 using NUnit.Framework;
 
@@ -7,7 +8,7 @@ using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 namespace fin.math {
   public class QuaternionUtilTests {
     [Test]
-    public void ToEulerDegrees() {
+    public void ToEulerRadians() {
       var degToRad = MathF.PI / 180;
 
       var x = 1 * 20 * degToRad;
@@ -21,6 +22,15 @@ namespace fin.math {
       Assert.AreEqual(x, v.X, FinTrig.PRECISION);
       Assert.AreEqual(y, v.Y, FinTrig.PRECISION);
       Assert.AreEqual(z, v.Z, FinTrig.PRECISION);
+    }
+
+    [Test]
+    public void ToEulerRadiansIdentity() {
+      var v = QuaternionUtil.ToEulerRadians(Quaternion.Identity);
+
+      Assert.AreEqual(0, v.X, FinTrig.PRECISION);
+      Assert.AreEqual(0, v.Y, FinTrig.PRECISION);
+      Assert.AreEqual(0, v.Z, FinTrig.PRECISION);
     }
   }
 }
