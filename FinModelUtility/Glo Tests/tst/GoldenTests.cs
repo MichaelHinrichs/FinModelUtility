1375ff784c94568f81dde87808835a2b11ffa0b3
MeltyPlayer <mastergamepro@hotmail.com>
2023-04-04 18:15:47 -0500

    Set up very basic test coverage ensuring that GLB files are exported as expected.

--- FinModelUtility/Glo Tests/tst/GoldenTests.cs
+++ FinModelUtility/Glo Tests/tst/GoldenTests.cs
@@ -1,8 +1,11 @@
 ﻿using System.Reflection;
 
+using fin.exporter;
+using fin.exporter.assimp.indirect;
 using fin.io;
 using fin.util.strings;
 
+using glo.api;
 using glo.schema;
 
 using NUnit.Framework;
@@ -15,17 +18,81 @@ using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 namespace glo {
   public class GoldenTests {
     [Test]
-    public async Task TestReadingAndWritingEachGloGolden() {
-      this.GetGloGoldens_(out _, out var gloGoldenFiles);
-      foreach (var goldenFile in gloGoldenFiles) {
-        var er = new EndianBinaryReader(goldenFile.OpenRead());
+    public async Task TestReadingAndWriting() {
+      this.GetGloGoldenBundles_(out _, out var gloGoldenBundles);
+      foreach (var goldenBundle in gloGoldenBundles) {
+        var er = new EndianBinaryReader(goldenBundle.GloFile.OpenRead());
         await BinarySchemaAssert.ReadsAndWritesIdentically<Glo>(er);
       }
     }
 
-    private void GetGloGoldens_(
+    [Test]
+    public void TestExportsExactModel() {
+      this.GetGloGoldenBundles_(out var gloGoldensDirectory,
+                                out var gloGoldenBundles);
+
+      var tmpDirectory =
+          (IDirectory) gloGoldensDirectory.Impl.GetSubdir("tmp", true);
+
+      var extensions = new[] {".glb"};
+
+      foreach (var goldenBundle in gloGoldenBundles) {
+        foreach (var file in tmpDirectory.GetExistingFiles()) {
+          file.Delete();
+        }
+
+        var inputDirectory = goldenBundle.GloFile.Parent;
+        var outputDirectory = inputDirectory.Parent.GetExistingSubdir("output");
+
+        var hasGoldenExport =
+            outputDirectory.Files.Any(
+                file => extensions.Contains(file.Extension));
+
+        var targetDirectory =
+            hasGoldenExport ? tmpDirectory : outputDirectory.Impl;
+
+        var model = new GloModelLoader().LoadModel(goldenBundle);
+        new AssimpIndirectExporter().ExportExtensions(
+            new ExporterParams {
+                Model = model,
+                OutputFile =
+                    new FinFile(Path.Combine(targetDirectory.FullName,
+                                             $"{goldenBundle.GloFile.NameWithoutExtension}.foo")),
+            },
+            extensions);
+
+        if (hasGoldenExport) {
+          AssertSameFiles_(tmpDirectory, outputDirectory.Impl);
+        }
+      }
+    }
+
+    private void AssertSameFiles_(IDirectory lhs, IDirectory rhs) {
+      var lhsFiles = lhs.GetExistingFiles().ToDictionary(file => (string) file.Name);
+      var rhsFiles = rhs.GetExistingFiles().ToDictionary(file => (string) file.Name);
+
+      Assert.IsTrue(lhsFiles.Keys.ToHashSet().SetEquals(rhsFiles.Keys.ToHashSet()));
+
+      foreach (var (name, lhsFile) in lhsFiles) {
+        var rhsFile = rhsFiles[name];
+
+        using var lhsStream = lhsFile.OpenRead();
+        using var rhsStream = rhsFile.OpenRead();
+
+        Assert.AreEqual(lhsStream.Length, rhsStream.Length);
+
+        for (var i = 0; i < lhsStream.Length; i++) {
+          //Assert.AreEqual(lhsStream.ReadByte(), rhsStream.ReadByte());
+          if (lhsStream.ReadByte() != rhsStream.ReadByte()) {
+            Assert.Fail();
+          }
+        }
+      }
+    }
+
+    private void GetGloGoldenBundles_(
         out IFileHierarchyDirectory gloGoldensDirectory,
-        out IEnumerable<IFileHierarchyFile> gloGoldenFiles) {
+        out IEnumerable<GloModelFileBundle> gloGoldenBundles) {
       var executingAssembly = Assembly.GetExecutingAssembly();
       var assemblyName =
           StringUtil.UpTo(executingAssembly.ManifestModule.Name, ".dll");
@@ -46,7 +113,13 @@ namespace glo {
       var hierarchy = new FileHierarchy(goldensDirectory);
 
       gloGoldensDirectory = hierarchy.Root;
-      gloGoldenFiles = gloGoldensDirectory.FilesWithExtension(".glo");
+      gloGoldenBundles = gloGoldensDirectory.Subdirs.Select(subdir => {
+        if (subdir.TryToGetExistingSubdir("input", out var inputDir)) {
+          var gloFile = inputDir.FilesWithExtension(".glo").Single();
+          return new GloModelFileBundle(gloFile, new[] { inputDir });
+        }
+        return null;
+      }).Where(bundle => bundle != null);
     }
   }
 }
