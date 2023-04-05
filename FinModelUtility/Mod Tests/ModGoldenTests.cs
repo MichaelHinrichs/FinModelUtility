169fd35b9fcf544152be93217a2e1fe890f6dd46
MeltyPlayer <mastergamepro@hotmail.com>
2023-04-04 20:44:18 -0500

    Set up golden tests for Mod exporting.

---
+++ FinModelUtility/Mod Tests/ModGoldenTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing;
+
+using mod.cli;
+
+using NUnit.Framework;
+
+
+namespace mod {
+  public class ModGoldenTests {
+    [Test]
+    public void TestExportsExactModel() {
+      ModelGoldenAssert.AssertExportGoldens(
+          this.GetGoldensDirectory_(),
+          new ModModelLoader(),
+          this.GetModModelFileBundleInDirectory_);
+    }
+
+    private IDirectory GetGoldensDirectory_()
+      => ModelGoldenAssert.GetRootGoldensDirectory(
+          Assembly.GetExecutingAssembly());
+
+    private ModModelFileBundle GetModModelFileBundleInDirectory_(
+        IFileHierarchyDirectory directory) {
+      return new ModModelFileBundle {
+          GameName = "pikmin_1",
+          ModFile = directory.FilesWithExtension(".mod").Single(),
+          AnmFile = directory.FilesWithExtension(".anm").Single(),
+      };
+    }
+  }
+}
