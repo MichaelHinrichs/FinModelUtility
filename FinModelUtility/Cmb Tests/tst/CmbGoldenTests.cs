db6591c31cc1be8d309790c95dd8221a961c5036
MeltyPlayer <mastergamepro@hotmail.com>
2023-04-04 20:50:23 -0500

    Set up goldens for BMDs.

--- FinModelUtility/Cmb Tests/tst/CmbGoldenTests.cs
+++ FinModelUtility/Cmb Tests/tst/CmbGoldenTests.cs
@@ -10,14 +10,10 @@ namespace cmb {
   public class CmbGoldenTests {
     [Test]
     public void TestExportsExactModel() {
-      var cmbGoldensDirectory = this.GetCmbGoldensDirectory_();
-      foreach (var subdir in cmbGoldensDirectory.GetExistingSubdirs()) {
-        var gameName = subdir.Name;
-        ModelGoldenAssert.AssertExportGoldens(
-            subdir,
-            new CmbModelLoader(),
-            dir => this.GetCmbModelFileBundleInDirectory_(dir, gameName));
-      }
+      ModelGoldenAssert.AssertExportGoldens(
+          this.GetCmbGoldensDirectory_(),
+          new CmbModelLoader(),
+          this.GetModelFileBundleInDirectory_);
     }
 
     private IDirectory GetCmbGoldensDirectory_()
@@ -25,12 +21,11 @@ namespace cmb {
                               Assembly.GetExecutingAssembly())
                           .GetSubdir("cmb");
 
-    private CmbModelFileBundle GetCmbModelFileBundleInDirectory_(
-        IFileHierarchyDirectory directory,
-        string gameName) {
+    private CmbModelFileBundle GetModelFileBundleInDirectory_(
+        IFileHierarchyDirectory directory) {
       var cmbFile = directory.FilesWithExtension(".cmb").Single();
       return new CmbModelFileBundle(
-          gameName,
+          "foobar",
           cmbFile,
           directory.FilesWithExtension(".csab").ToArray(),
           null,
---
+++ FinModelUtility/J3d Tests/BmdGoldenTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing;
+
+using j3d.exporter;
+
+
+namespace j3d {
+  public class BmdGoldenTests {
+    [Test]
+    public void TestExportsExactModel() {
+      ModelGoldenAssert.AssertExportGoldens(
+          this.GetGoldensDirectory_(),
+          new BmdModelLoader(),
+          this.GetModelFileBundleInDirectory_);
+    }
+
+    private IDirectory GetGoldensDirectory_()
+      => ModelGoldenAssert.GetRootGoldensDirectory(
+          Assembly.GetExecutingAssembly());
+
+    private BmdModelFileBundle GetModelFileBundleInDirectory_(
+        IFileHierarchyDirectory directory) {
+      return new BmdModelFileBundle {
+          GameName = "foobar",
+          BmdFile = directory.FilesWithExtension(".bmd").Single(),
+          BcxFiles = directory.FilesWithExtensions(".bca", ".bck").ToArray(),
+          BtiFiles = directory.FilesWithExtension(".bti").ToArray(),
+      };
+    }
+  }
+}
