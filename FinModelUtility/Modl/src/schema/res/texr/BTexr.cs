3884a9665e2652ffa1bdfaf7608179750b7cdd7b
MeltyPlayer <mastergamepro@hotmail.com>
2023-04-19 17:19:08 -0500

    Switched Battalion Wars to use the new image loading methods.

--- FinModelUtility/Modl/src/schema/res/texr/BTexr.cs
+++ FinModelUtility/Modl/src/schema/res/texr/BTexr.cs
@@ -1,9 +1,15 @@
 ﻿using fin.color;
 using fin.data;
 using fin.image;
+using fin.image.io;
+using fin.image.io.tile;
+using fin.io.image.tile;
 using fin.util.color;
 
 using schema.binary;
+
+using SixLabors.ImageSharp.PixelFormats;
+
 
 namespace modl.schema.res.texr {
   public interface ITexr : IBinaryDeserializable {
@@ -70,76 +76,9 @@ namespace modl.schema.res.texr {
       var mipSize = width * height / 2;
       SectionHeaderUtil.AssertNameAndSize(er, "MIP ", mipSize);
 
-      var tileWidth = 4;
-      var tileHeight = 4;
-
-      IColor[] colors = new IColor[4];
-
-      var image = new Rgba32Image(PixelFormat.DXT1, (int) width, (int) height);
-      image.Mutate((_, setHandler) => {
-        var x = 0;
-        var y = 0;
-
-        for (var ii = 0; ii < mipSize / 8; ++ii) {
-          var color0Value = er.ReadUInt16();
-          var color1Value = er.ReadUInt16();
-          var pixelMask = er.ReadUInt32();
-
-          var color0 = colors[0] = ColorUtil.ParseRgb565(color0Value);
-          var color1 = colors[1] = ColorUtil.ParseRgb565(color1Value);
-
-          if (color0Value > color1Value) {
-            colors[2] = FinColor.FromRgbBytes(
-                (byte) ((2 * color0.Rb + color1.Rb) / 3f),
-                (byte) ((2 * color0.Gb + color1.Gb) / 3f),
-                (byte) ((2 * color0.Bb + color1.Bb) / 3f));
-            colors[3] = FinColor.FromRgbBytes(
-                (byte) ((2 * color1.Rb + color0.Rb) / 3f),
-                (byte) ((2 * color1.Gb + color0.Gb) / 3f),
-                (byte) ((2 * color1.Bb + color0.Bb) / 3f));
-          } else {
-            colors[2] = FinColor.FromRgbBytes(
-                (byte) ((color0.Rb + color1.Rb) / 2f),
-                (byte) ((color0.Gb + color1.Gb) / 2f),
-                (byte) ((color0.Bb + color1.Bb) / 2f));
-            colors[3] = FinColor.FromRgbaBytes(
-                0, 0, 0, 0);
-          }
-
-          var ii2 = ii % 4;
-          var iiX = (ii2 % 2) * 4;
-          var iiY = (ii2 / 2) * 4;
-
-          for (var yInTile = 0; yInTile < tileHeight; ++yInTile) {
-            var arrayY = y + iiY + yInTile;
-            for (var xInTile = 0; xInTile < tileWidth; ++xInTile) {
-              var arrayX = x + iiX + xInTile;
-
-              var colorIndex =
-                  (pixelMask >> ((15 - (yInTile * 4 + xInTile)) * 2)) &
-                  0b11;
-              var color = colors[colorIndex];
-
-              setHandler(arrayX,
-                         arrayY,
-                         color.Rb,
-                         color.Gb,
-                         color.Bb,
-                         color.Ab);
-            }
-          }
-
-          if (ii2 == 3) {
-            x += 8;
-            if (x >= width) {
-              x = 0;
-              y += 8;
-            }
-          }
-        }
-      });
-
-      return image;
+      return TiledImageReader
+             .New((int) width, (int) height, new CmprTileReader(), er.Endianness)
+             .Read(er.ReadBytes(mipSize));
     }
 
     protected IImage ReadP8_(IEndianBinaryReader er, uint width, uint height) {
@@ -244,129 +183,50 @@ namespace modl.schema.res.texr {
 
     protected IImage ReadIA8_(IEndianBinaryReader er, uint width, uint height) {
       SectionHeaderUtil.AssertNameAndSize(er, "MIP ", 2 * width * height);
-
-      var image = new La16Image(PixelFormat.LA88, (int) width, (int) height);
-      image.Mutate((_, setHandler) => {
-        var blockWidth = 4;
-        var blockHeight = 4;
-
-        var blockCountX = width / blockWidth;
-        var blockCountY = height / blockHeight;
-
-        for (var blockY = 0; blockY < blockCountY; ++blockY) {
-          for (var blockX = 0; blockX < blockCountX; ++blockX) {
-            for (var yInBlock = 0; yInBlock < blockHeight; ++yInBlock) {
-              var y = blockY * blockHeight + yInBlock;
-              for (var xInBlock = 0; xInBlock < blockWidth; ++xInBlock) {
-                var x = blockX * blockWidth + xInBlock;
-
-                var intensity = er.ReadByte();
-                var alpha = er.ReadByte();
-
-                setHandler(x, y, intensity, alpha);
-              }
-            }
-          }
-        }
-      });
-
-      return image;
+      return TiledImageReader
+             .New((int) width,
+                  (int) height,
+                  4,
+                  4,
+                  new La16PixelReader(),
+                  er.Endianness)
+             .Read(er.ReadBytes(2 * width * height));
     }
 
     protected IImage ReadIA4_(IEndianBinaryReader er, uint width, uint height) {
       SectionHeaderUtil.AssertNameAndSize(er, "MIP ", width * height);
-
-      var image = new La16Image(PixelFormat.LA44, (int) width, (int) height);
-      image.Mutate((_, setHandler) => {
-        var blockWidth = 8;
-        var blockHeight = 4;
-
-        var blockCountX = width / blockWidth;
-        var blockCountY = height / blockHeight;
-
-        for (var blockY = 0; blockY < blockCountY; ++blockY) {
-          for (var blockX = 0; blockX < blockCountX; ++blockX) {
-            for (var yInBlock = 0; yInBlock < blockHeight; ++yInBlock) {
-              var y = blockY * blockHeight + yInBlock;
-              for (var xInBlock = 0; xInBlock < blockWidth; ++xInBlock) {
-                var x = blockX * blockWidth + xInBlock;
-
-                var color = er.ReadByte();
-
-                var intensity = ColorUtil.ExtractScaled(color, 0, 4);
-                var alpha = ColorUtil.ExtractScaled(color, 4, 4);
-
-                setHandler(x, y, intensity, alpha);
-              }
-            }
-          }
-        }
-      });
-
-      return image;
+      return TiledImageReader
+             .New((int) width,
+                  (int) height,
+                  8,
+                  4,
+                  new La8PixelReader(),
+                  er.Endianness)
+             .Read(er.ReadBytes(width * height));
     }
 
     protected IImage ReadI8_(IEndianBinaryReader er, uint width, uint height) {
       SectionHeaderUtil.AssertNameAndSize(er, "MIP ", width * height);
-
-      var image = new L8Image(PixelFormat.L8, (int) width, (int) height);
-      image.Mutate((_, setHandler) => {
-        var blockWidth = 8;
-        var blockHeight = 4;
-
-        var blockCountX = width / blockWidth;
-        var blockCountY = height / blockHeight;
-
-        for (var blockY = 0; blockY < blockCountY; ++blockY) {
-          for (var blockX = 0; blockX < blockCountX; ++blockX) {
-            for (var yInBlock = 0; yInBlock < blockHeight; ++yInBlock) {
-              var y = blockY * blockHeight + yInBlock;
-              for (var xInBlock = 0; xInBlock < blockWidth; ++xInBlock) {
-                var x = blockX * blockWidth + xInBlock;
-
-                var intensity = er.ReadByte();
-                setHandler(x, y, intensity);
-              }
-            }
-          }
-        }
-      });
-
-      return image;
+      return TiledImageReader
+             .New((int) width,
+                  (int) height,
+                  8,
+                  4,
+                  new L8PixelReader(),
+                  er.Endianness)
+             .Read(er.ReadBytes(width * height));
     }
 
     protected IImage ReadI4_(IEndianBinaryReader er, uint width, uint height) {
       SectionHeaderUtil.AssertNameAndSize(er, "MIP ", width * height / 2);
-
-      var image = new L8Image(PixelFormat.L4, (int) width, (int) height);
-      image.Mutate((_, setHandler) => {
-        var blockWidth = 8;
-        var blockHeight = 8;
-
-        var blockCountX = width / blockWidth;
-        var blockCountY = height / blockHeight;
-
-        for (var blockY = 0; blockY < blockCountY; ++blockY) {
-          for (var blockX = 0; blockX < blockCountX; ++blockX) {
-            for (var yInBlock = 0; yInBlock < blockHeight; ++yInBlock) {
-              var y = blockY * blockHeight + yInBlock;
-              for (var xInBlock = 0; xInBlock < blockWidth; xInBlock += 2) {
-                var x = blockX * blockWidth + xInBlock;
-
-                var color = er.ReadByte();
-
-                var intensity1 = ColorUtil.ExtractScaled(color, 4, 4);
-                var intensity2 = ColorUtil.ExtractScaled(color, 0, 4);
-
-                setHandler(x, y, intensity1);
-                setHandler(x + 1, y, intensity2);
-              }
-            }
-          }
-        }
-      });
-
-      return image;
+      return TiledImageReader
+             .New((int) width,
+                  (int) height,
+                  8,
+                  8,
+                  new L8PixelReader(),
+                  er.Endianness)
+             .Read(er.ReadBytes(width * height / 2));
     }
   }
 }
