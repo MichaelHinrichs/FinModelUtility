dc8770b6e7fe554d477144532c36b54c5f39c723
MeltyPlayer <mastergamepro@hotmail.com>
2023-05-03 06:57:13 -0500

    Copied over the highly accurate task logic from libsm64sharp.

---
+++ FinModelUtility/Fin/src/util/tasks/TaskUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace fin.util.tasks {
+  public static class TaskUtil {
+    public static Task RunExpensiveButAccurateTickHandler(
+        double frequency,
+        Action handler,
+        CancellationToken? cancellationToken = null)
+      => Task.Run(() => {
+        var stopwatch = new Stopwatch();
+        var targetPeriod = 1 / frequency;
+        var targetTicks = Stopwatch.Frequency * targetPeriod;
+
+        while (!cancellationToken?.IsCancellationRequested ?? true) {
+          stopwatch.Restart();
+
+          handler();
+
+          // Very expensive, but FAR more accurate than Thread.sleep
+          var i = 0;
+          while (stopwatch.ElapsedTicks < targetTicks) {
+            ++i;
+          }
+        }
+      });
+  }
+}
