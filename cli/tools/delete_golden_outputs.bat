8b3f5d38c10b1d0f3883c5f5ada633e7565e7cd0
MeltyPlayer <mastergamepro@hotmail.com>
2023-05-04 19:03:20 -0500

    Updated the goldens and set up a batch script for auto deleting all goldens.

--- cli/tools/delete_golden_outputs.bat
+++ cli/tools/delete_golden_outputs.bat
@@ -1,22 +1,35 @@
+@echo off 
 echo This will delete all goldens output files. Are you sure you wish to proceed?
 
 pause
 
 cd ../../FinModelUtility
 
-for /D %%p in (*) do ( 
+set hierarchyListCmd="dir /b /s /ad *.* | sort"
+
+for /d %%p in (*) do ( 
+  pushd "./"
+
   cd "%%p"
-  
+    
   if exist goldens\ (
-    set hierarchyListCmd="dir /b /s /ad *.* | sort"
     for /f "tokens=*" %%d in ('%hierarchyListCmd%') do (
+      pushd "./"
+	
+      cd "%%d"
+	
       if exist input\ (
       	if exist output\ (
-          cd output
-		  
-		  del *.*
+          cd output\
+		  del /q *.*
   	    )
       )
+	  
+	  popd
 	)
   )
+
+  popd
 )
+
+pause
