69e4053cb7bcd774b1b83f9293a58e8755526e57
MeltyPlayer <mastergamepro@hotmail.com>
2023-05-31 07:04:55 -0500

    Added some basic test coverage for creating quaternions.

--- FinModelUtility/Fin/Fin Tests/math/QuaternionUtilTests.cs
+++ FinModelUtility/Fin/Fin Tests/math/QuaternionUtilTests.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Numerics;
 
+using MathNet.Numerics;
+
 using NUnit.Framework;
 
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace fin.math {
   public class QuaternionUtilTests {
-    private float PRECISION = .0001f;
+    private float PRECISION = .01f;
+
+    [Test]
+    [TestCase(0, 0, 0, 0, 0, 0, 1)]
+    [TestCase(2 * MathF.PI, 2 * MathF.PI, 2 * MathF.PI, 0, 0, 0, -1)]
+    [TestCase(1, 2, 3, -0.71f, 0.31f, 0.44f, 0.43f)]
+    [TestCase(2, 3, 4, -.51f, -.17f, .38f, .74f)]
+    public void TestCreateZyx(float xRadians,
+                              float yRadians,
+                              float zRadians,
+                              float expectedQX,
+                              float expectedQY,
+                              float expectedQZ,
+                              float expectedQW) {
+      var actualQuaternion = QuaternionUtil.CreateZyx(xRadians, yRadians, zRadians);
+
+      if (!actualQuaternion.X.AlmostEqual(expectedQX, this.PRECISION) ||
+          !actualQuaternion.Y.AlmostEqual(expectedQY, this.PRECISION) ||
+          !actualQuaternion.Z.AlmostEqual(expectedQZ, this.PRECISION) ||
+          !actualQuaternion.W.AlmostEqual(expectedQW, this.PRECISION)) {
+        Assert.Fail($"Expected {actualQuaternion} to equal {new Quaternion(expectedQX, expectedQY, expectedQZ, expectedQW)}");
+      }
+    }
 
     [Test]
     public void ToEulerRadians() {
