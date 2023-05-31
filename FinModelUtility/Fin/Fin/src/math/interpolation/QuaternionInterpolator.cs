2ce6ebe86e6b12e7c2b36f3d4e3ca7f229cd40db
MeltyPlayer <mastergamepro@hotmail.com>
2023-05-31 10:15:50 -0500

    Added a new interpolator for quaternions.

---
+++ FinModelUtility/Fin/Fin/src/math/interpolation/QuaternionInterpolator.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace fin.math.interpolation {
+  public readonly struct QuaternionInterpolator : IInterpolator<Quaternion> {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Quaternion Interpolate(
+        Quaternion q1,
+        Quaternion q2,
+        float progress) {
+      if (Quaternion.Dot(q1, q2) < 0) {
+        q2 = -q2;
+      }
+
+      var interp = Quaternion.Slerp(q1, q2, progress);
+      return Quaternion.Normalize(interp);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Quaternion Interpolate(float fromTime,
+                                  Quaternion fromValue,
+                                  float fromTangent,
+                                  float toTime,
+                                  Quaternion toValue,
+                                  float toTangent,
+                                  float time) {
+      // TODO: Figure out how to use tangents here
+      var t = (time - fromTime) / (toTime - fromTime);
+      return Interpolate(fromValue, toValue, t);
+    }
+  }
+}
