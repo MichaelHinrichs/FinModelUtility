b2841207241fb0f97a91138bc8afc76b465b5e49
MeltyPlayer <mastergamepro@hotmail.com>
2023-06-02 19:59:52 -0500

    Added some more golden files for additional J3d coverage.

---
+++ FinModelUtility/J3d/J3d Tests/goldens/pikmin_2_BigFoot/output/ashi_mat_fragment_shader.glsl
@@ -0,0 +1,50 @@
+# version 330
+
+uniform sampler2D texture0;
+uniform sampler2D texture2;
+
+struct Light {
+  bool enabled;
+  vec3 position;
+  vec3 normal;
+  vec4 color;
+};
+
+uniform Light lights[8];
+
+in vec2 normalUv;
+in vec3 vertexNormal;
+in vec4 vertexColor0;
+in vec4 vertexColor1;
+in vec2 uv0;
+
+out vec4 fragColor;
+
+vec4 getLightColor(Light light) {
+  if (!light.enabled) {
+    return vec4(0);
+  }
+
+  vec3 diffuseLightNormal = normalize(light.normal);
+  float diffuseLightAmount = max(-dot(vertexNormal, diffuseLightNormal), 0);
+  float lightAmount = min(diffuseLightAmount, 1);
+  return lightAmount * light.color;
+}
+
+void main() {
+  vec4 individualLightColors[8];
+  for (int i = 0; i < 8; ++i) {
+    vec4 lightColor = getLightColor(lights[i]);
+    individualLightColors[i] = lightColor;
+  }
+
+  vec3 colorComponent = clamp(vec3(0.5)*texture(texture2, acos(normalUv) / 3.14159).rgb + vec3(0.5) + clamp((individualLightColors[0].rgb + vec3(0.19607843137254902)), 0, 1)*texture(texture0, uv0).rgb*vec3(2)*clamp((vec3(0.5)*texture(texture2, acos(normalUv) / 3.14159).rgb + vec3(0.5)), 0, 1) + vec3(-0.5), 0, 1);
+
+  float alphaComponent = texture(texture0, uv0).a + -1*(1 + -1*0.2823529541492462);
+
+  fragColor = vec4(colorComponent, alphaComponent);
+
+  if (!(fragColor.a >= 0.003921569)) {
+    discard;
+  }
+}
---
+++ FinModelUtility/J3d/J3d Tests/goldens/pikmin_2_flower_red/output/flower1_v_fragment_shader.glsl
@@ -0,0 +1,42 @@
+# version 330
+
+
+struct Light {
+  bool enabled;
+  vec3 position;
+  vec3 normal;
+  vec4 color;
+};
+
+uniform Light lights[8];
+
+in vec3 vertexNormal;
+in vec4 vertexColor0;
+in vec4 vertexColor1;
+
+out vec4 fragColor;
+
+vec4 getLightColor(Light light) {
+  if (!light.enabled) {
+    return vec4(0);
+  }
+
+  vec3 diffuseLightNormal = normalize(light.normal);
+  float diffuseLightAmount = max(-dot(vertexNormal, diffuseLightNormal), 0);
+  float lightAmount = min(diffuseLightAmount, 1);
+  return lightAmount * light.color;
+}
+
+void main() {
+  vec4 individualLightColors[8];
+  for (int i = 0; i < 8; ++i) {
+    vec4 lightColor = getLightColor(lights[i]);
+    individualLightColors[i] = lightColor;
+  }
+
+  vec3 colorComponent = clamp(vertexColor0.rgb*clamp((individualLightColors[0].rgb + vec3(0.19607843137254902)), 0, 1)*vec3(0.9019607901573181)*vec3(4), 0, 1);
+
+  float alphaComponent = vertexColor0.a;
+
+  fragColor = vec4(colorComponent, alphaComponent);
+}
