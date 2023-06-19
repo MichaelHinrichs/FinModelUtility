9481cf7071b397354e1e494b5c3c1538198d886a
MeltyPlayer <mastergamepro@hotmail.com>
2023-06-19 01:13:59 -0500

    Uploaded some files for extracting CIA files with ctrtool.

---
+++ cli/tools/ctrtool/extract_cia.bat
@@ -0,0 +1,27 @@
+@echo off
+
+if "%~1" == "" (goto noinput)
+
+if exist %~1 (goto validinput)
+
+:doesntexist
+echo Input CIA does not exist!
+goto end
+
+:noinput
+echo No input CIA!
+goto end
+
+:validinput
+ctrtool --contents=contents %~1
+if not exist contents.0000.00000000 (goto ciaextractfailed)
+rename contents.0000.00000000 nsmb2.cxi
+ctrtool --romfsdir=romfs --exefsdir=exefs --exheader=exheader.bin nsmb2.cxi
+for %%f in (contents.000*) do (del "%%f")
+goto end
+
+:ciaextractfailed
+echo Extracting CIA failed!
+goto end
+
+:end
