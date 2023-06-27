44b39cca14e61460721b6ceaabb725d59a02cf3b
MeltyPlayer <mastergamepro@hotmail.com>
2023-06-26 23:29:02 -0500

    Created a helper method for returning a value from a subread.

---
+++ FinModelUtility/Fin/Fin/src/util/binary/EndianBinaryReaderUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace fin.util.binary {
+  public static class EndianBinaryReaderUtil {
+    public static T Subread<T>(this IEndianBinaryReader er,
+                               long offset,
+                               Func<IEndianBinaryReader, T> handler) {
+      T value = default;
+      er.Subread(
+          offset,
+          ser => {
+            value = handler(ser);
+          });
+
+      return value!;
+    }
+  }
+}
