a21564c2c16ff2a3fd113de073463c08bc79485a
MeltyPlayer <mastergamepro@hotmail.com>
2023-08-16 19:32:41 -0500

    Split out the test cases for the Bmd and Mod tests.

---
+++ FinModelUtility/Mod/Mod Tests/ModModelGoldenTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing.model;
+
+using mod.cli;
+
+using NUnit.Framework;
+
+namespace mod {
+  public class ModModelGoldenTests
+      : BModelGoldenTests<ModModelFileBundle, ModModelLoader> {
+    [Test]
+    [TestCaseSource(nameof(GetGoldenDirectories_))]
+    public void TestExportsGoldenAsExpected(
+        IFileHierarchyDirectory goldenDirectory)
+      => this.AssertGolden(goldenDirectory);
+
+    public override ModModelFileBundle GetFileBundleFromDirectory(
+        IFileHierarchyDirectory directory) {
+      return new ModModelFileBundle {
+          GameName = "pikmin_1",
+          ModFile = directory.FilesWithExtension(".mod").Single(),
+          AnmFile = directory.FilesWithExtension(".anm").Single(),
+      };
+    }
+
+    private static IFileHierarchyDirectory[] GetGoldenDirectories_() {
+      var rootGoldenDirectory
+          = ModelGoldenAssert
+              .GetRootGoldensDirectory(Assembly.GetExecutingAssembly());
+      return ModelGoldenAssert.GetGoldenDirectories(rootGoldenDirectory)
+                              .ToArray();
+    }
+  }
+}
