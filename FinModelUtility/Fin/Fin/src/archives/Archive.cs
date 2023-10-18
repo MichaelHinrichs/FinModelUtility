5b80fab8f2dc7b4b15363d06e70bd7b7f6781e01
MeltyPlayer <mastergamepro@hotmail.com>
2023-10-18 01:53:48 -0500

    Set up a new commented-out archive class.

---
+++ FinModelUtility/Fin/Fin/src/archives/Archive.cs
@@ -0,0 +1,20 @@
+﻿namespace fin.io {
+  public readonly struct ArchiveFile {
+    public required string LocalPath { get; init; }
+    public required int Offset { get; init; }
+    public required int Length { get; init; }
+  }
+
+  public class Archive {
+    /*public Archive(IList<ArchiveFile> archiveFiles) {
+      var sortedFiles =
+          archiveFiles.OrderBy(archiveFile => archiveFile.LocalPath).ToArray();
+
+      foreach (var archiveFile in sortedFiles) { }
+    }
+
+    private readonly struct ArchiveDirectory : IReadOnlyTreeDirectory { }
+
+    private readonly struct ArchiveFile : IReadOnlyTreeFile { }*/
+  }
+}
