440e38f977f2de1524883d1a046d3c6cb5fa7d01
MeltyPlayer <mastergamepro@hotmail.com>
2023-10-27 20:59:36 -0500

    Deleted the lines pointing out from each bone in the skeleton renderer, because these are just distracting and aren't very robust.

--- FinModelUtility/Fin/Fin.Ui/src/rendering/gl/model/SkeletonRenderer.cs
+++ FinModelUtility/Fin/Fin.Ui/src/rendering/gl/model/SkeletonRenderer.cs
@@ -74,55 +74,6 @@ namespace fin.ui.rendering.gl.model {
         GL.End();
       }
 
-      // Renders lines out from each bone in its direction.
-      {
-        GL.LineWidth(5);
-        GL.Begin(PrimitiveType.Lines);
-
-        GL.Color4(1f, 0, 0, 1);
-
-        foreach (var bone in this.Skeleton) {
-          if (bone == rootBone || bone == this.SelectedBone) {
-            continue;
-          }
-
-          var from = new Vector3();
-          this.boneTransformManager_.ProjectPosition(bone, ref from);
-
-          var normal = new Vector3(1, 0, 0);
-          this.boneTransformManager_.ProjectNormal(bone, ref normal);
-
-          var normalScale = 50f / this.Scale;
-          normal = Vector3.Multiply(normal, normalScale);
-
-          GL.Vertex3(Unsafe.As<Vector3, OpenTK.Vector3>(ref from));
-          GL.Vertex3(from.X + normal.X, from.Y + normal.Y, from.Z + normal.Z);
-        }
-
-        GL.End();
-
-        if (this.SelectedBone != null) {
-          GL.LineWidth(8);
-          GL.Begin(PrimitiveType.Lines);
-
-          GL.Color4(1f, 1f, 1f, 1);
-
-          var from = new Vector3();
-          this.boneTransformManager_.ProjectPosition(this.SelectedBone, ref from);
-
-          var normal = new Vector3(1, 0, 0);
-          this.boneTransformManager_.ProjectNormal(this.SelectedBone, ref normal);
-
-          var normalScale = 50f / this.Scale;
-          normal = Vector3.Multiply(normal, normalScale);
-
-          GL.Vertex3(Unsafe.As<Vector3, OpenTK.Vector3>(ref from));
-          GL.Vertex3(from.X + normal.X, from.Y + normal.Y, from.Z + normal.Z);
-
-          GL.End();
-        }
-      }
-
       // Renders points at the start of each bone.
       {
         GL.PointSize(8);
